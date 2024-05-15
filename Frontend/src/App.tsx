9b50df6b1d9a0100d00a15fede67fadef21892d9
ducna34806 <ducnaph34806@fpt.edu.vn>
2024-05-15 19:41:12 +0700

    header1

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import RouterComponent from "./routes";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Header from "./components/Header";
 const App = () => {
   return (
     <>
       <RouterComponent />
       <ToastContainer />
+      <Header />
     </>
   );
 };
