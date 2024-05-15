90ba6cd9b49bf03a25cecd485f1d0706f5fa26ed
Trunghd09 <hoangductrung09112004@gmail.com>
2024-05-15 21:11:45 +0700

    listcart and Favourite

--- Frontend/src/routes/index.tsx
+++ Frontend/src/routes/index.tsx
@@ -1,4 +1,6 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import ListCart from "../pages/Client/Cart/ListCart";
+import Favourite from "../pages/Client/Favourite/Favourite";
 
 
 const RouterComponent = () => {
@@ -6,14 +8,15 @@ const RouterComponent = () => {
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<h1>Đây Là Trang Home</h1>}>
+          <Route path="/" element={<h1>Đây Là Trang Home</h1>} />
             <Route index element={<h1>Home</h1>} />
-            <Route path="/cart" element={<h1>Cart</h1>} />
-          </Route>
-          <Route path="/admin" element={<h1>Đây là Trang Admin</h1>}>
+            <Route path="cart" element={<ListCart />} />
+            <Route path="favourite" element={<Favourite />} />
+          
+          <Route path="/admin" element={<h1>Đây là Trang Admin</h1>} />
             <Route index element={<h1>Dashboard</h1>} />
             <Route path="/admin/product" element={<h1>Products</h1>} />
-          </Route>
+          
         </Routes>
       </Router>
     </>
