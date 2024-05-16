39fce80c17244876464bf9e98af81ccbc15f8d8e
gamnt04 <gamsau2004@gmail.com>
2024-05-16 20:06:09 +0700

    push all

--- Frontend/src/routes/index.tsx
+++ Frontend/src/routes/index.tsx
@@ -7,7 +7,7 @@ const RouterComponent = () => {
         <Routes>
           <Route path="/" element={<h1>Hello</h1>}>
             <Route index element={<h1>Hello2</h1>} />
-            <Route path="/cart" element={<h1>Cart</h1>} />
+            <Route path="/cart" element={<h1>Cart1</h1>} />
           </Route>
           <Route path="/admin" element={<h1>Đây là Trang Admin</h1>}>
             <Route index element={<h1>Dashboard</h1>} />
