d457668a6b138aa6b4ba3296833c94e5802be2fa
BaoChung-Cloud <chungkbph38477@fpt.edu.vn>
2024-05-17 18:59:23 +0700

    new

--- Frontend/src/routes/index.tsx
+++ Frontend/src/routes/index.tsx
@@ -1,30 +1,32 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-<<<<<<< HEAD
-import Blogs from "../pages/Client/Blogs/Blogs";
-=======
 
 import ListCart from "../pages/Client/Cart/ListCart";
 import Favourite from "../pages/Client/Favourite/Favourite";
 import FAQ from "../pages/Client/Pages/FAQ/FAQs";
 import AboutUS from "../pages/Client/Pages/About-us/About_us";
 import Delivery from "../pages/Client/Pages/Delivery/Delivery";
+import ListProducts from "../pages/Client/listProducts/ListProducts";
 import Header from "../components/Header";
-import Footer from "../components/Footer";
 
-
->>>>>>> a80637914551f8a1453387b71fc72ac43ec57561
 const RouterComponent = () => {
   return (
     <>
       <Router>
-      <Header></Header>
- 
+        <Header></Header>
         <Routes>
           <Route path="/" element={<h1>Đây Là Trang Home</h1>} />
           <Route index element={<h1>Home</h1>} />
-          <Route path="/blogs" element={<Blogs />} />
+          <Route path="cart" element={<ListCart />} />
+          <Route path="favourite" element={<Favourite />} />
+          <Route path="delivery" element={<Delivery />} />
+          <Route path="faqs" element={<FAQ />} />
+          <Route path="about-us" element={<AboutUS />} />
+          <Route path="/products" element={<ListProducts />} />
+
+          <Route path="/admin" element={<h1>Đây là Trang Admin</h1>} />
+          <Route index element={<h1>Dashboard</h1>} />
+          <Route path="/admin/product" element={<h1>Products</h1>} />
         </Routes>
-        <Footer></Footer>
       </Router>
     </>
   );
