c45b7c65f117aa7649f00a27d8049e06782e3eab
duonghainam1 <namhai0301004@gmail.com>
2024-05-29 13:31:01 +0700

    BE base lại

---
+++ BE/src/models/Category.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+import slug from "mongoose-slug-updater";
+mongoose.plugin(slug);
+
+const categorySchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    products: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+      },
+    ],
+    collections: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Collection",
+      },
+    ],
+    slug: {
+      type: String,
+      slug: "name",
+      unique: true,
+    },
+  },
+
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+export default mongoose.model("Category", categorySchema);
