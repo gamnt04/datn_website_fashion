5e46a6688872c96e6024e7d34fa99564cf4d1bb3
duonghainam1 <namhai0301004@gmail.com>
2024-05-29 13:31:01 +0700

    BE base lại

---
+++ BE/src/controllers/auth.js
@@ -0,0 +1,63 @@
+import bcryptjs from "bcryptjs";
+import { StatusCodes } from "http-status-codes";
+
+import jwt from "jsonwebtoken";
+import User from "../models/users";
+import { signupSchema } from "../validations/auth";
+
+
+
+export const signup = async (req, res) => {
+    const { email, password, name, avatar } = req.body;
+    const { error } = signupSchema.validate(req.body, { abortEarly: false });
+    console.log(error);
+    if (error) {
+        const messages = error.details.map((item) => item.message);
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+            messages,
+        });
+    }
+
+    const existUser = await User.findOne({ email });
+    if (existUser) {
+        return res.status(StatusCodes.BAD_REQUEST).json({
+            messages: ["Email đã tồn tại"],
+        });
+    }
+
+    const hashedPassword = await bcryptjs.hash(password, 12);
+    const role = (await User.countDocuments({})) === 0 ? "admin" : "user";
+
+    const user = await User.create({
+        ...req.body,
+        password: hashedPassword,
+        role,
+    });
+
+    return res.status(StatusCodes.CREATED).json({
+        user,
+    });
+};
+export const signin = async (req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+        return res.status(StatusCodes.NOT_FOUND).json({
+            messages: ["Email không tồn tại"],
+        });
+    }
+    const isMatch = await bcryptjs.compare(password, user.password);
+    if (!isMatch) {
+        return res.status(StatusCodes.BAD_REQUEST).json({
+            messages: ["Mật khẩu không chính xác"],
+        });
+    }
+    const token = jwt.sign({ userId: user._id }, "123456", {
+        expiresIn: "7d",
+    });
+    return res.status(StatusCodes.OK).json({
+        user,
+        token,
+    });
+};
+export const logout = async (req, res) => { };
---
+++ BE/src/routers/orders.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import { createOrder, getOrderById, getOrders } from "../controllers/orders";
+
+const router = Router()
+router.post("/orders", createOrder);
+router.get("/orders", getOrders);
+router.get("/orders/:userId/:orderId", getOrderById);
+export default router
