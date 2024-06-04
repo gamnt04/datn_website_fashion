87b50e9d036b45a36683fd575e7ae176147cde07
BaoChung-Cloud <chungkbph38477@fpt.edu.vn>
2024-06-04 21:22:22 +0700

    cart 6/4

---
+++ BE/src/models/cart.js
@@ -0,0 +1,26 @@
+import mongoose, { Schema } from "mongoose";
+
+const cartSchema = new Schema(
+  {
+    userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    product: [
+      {
+        productId: {
+          type: Schema.Types.ObjectId,
+          ref: "Products",
+          required: true
+        },
+        quantiy: {
+          type: Number,
+          required: true
+        }
+      }
+    ]
+  },
+  {
+    timestamps: true,
+    versionKey: false
+  }
+);
+
+export default mongoose.model("Cart", cartSchema);
