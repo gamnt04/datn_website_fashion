9784b27e6c6a1306044c1b8c8e1fbf958f00fd5b
duonghainam1 <namhai0301004@gmail.com>
2024-06-24 14:11:07 +0700

    client allOrders

--- FE/src/pages/Client/(Home)/[Brand].tsx
+++ FE/src/pages/Client/(Home)/[Brand].tsx
@@ -1,39 +1,41 @@
 
 const Brand = () => {
   return (<>
-    <div className="grid grid-cols-1 gap-20 sm:grid-cols-2 md:grid-cols-6 *:cursor-pointer overflow-hidden border-b">
+    <div className="border-b">
+      <div className="container mx-auto grid grid-cols-2 lg:grid lg:grid-cols-3 xl:flex xl:gap-10 *:cursor-pointer overflow-hidden">
         <img
-          className="w-[200px] h-[170px] transition-transform duration-300 transform hover:scale-105"
+          className="w-[150px] h-[130px] xl:w-[200px] xl:h-[170px] transition-transform duration-300 transform hover:scale-105"
           src="https://stark-ver2.myshopify.com/cdn/shop/files/brand1.jpg?v=1693474045&width=200"
           alt=""
         />
         <img
-          className="w-[200px] h-[170px] transition-transform duration-300 transform hover:scale-105"
+          className="w-[150px] h-[130px] xl:w-[200px] xl:h-[170px] transition-transform duration-300 transform hover:scale-105"
           src="https://stark-ver2.myshopify.com/cdn/shop/files/brand2.jpg?v=1693474055&width=200"
           alt=""
         />
         <img
-          className="w-[200px] h-[170px] transition-transform duration-300 transform hover:scale-105"
+          className="w-[150px] h-[130px] xl:w-[200px] xl:h-[170px] transition-transform duration-300 transform hover:scale-105"
           src="https://stark-ver2.myshopify.com/cdn/shop/files/brand3.jpg?v=1693474061&width=200"
           alt=""
         />
         <img
-          className="w-[200px] h-[170px] transition-transform duration-300 transform hover:scale-105"
+          className="w-[150px] h-[130px] xl:w-[200px] xl:h-[170px] transition-transform duration-300 transform hover:scale-105"
           src="https://stark-ver2.myshopify.com/cdn/shop/files/brand4.jpg?v=1693474068&width=200"
           alt=""
         />
         <img
-          className="w-[200px] h-[170px] transition-transform duration-300 transform hover:scale-105"
+          className="w-[150px] h-[130px] xl:w-[200px] xl:h-[170px] transition-transform duration-300 transform hover:scale-105"
           src="https://stark-ver2.myshopify.com/cdn/shop/files/brand5.jpg?v=1693474075&width=200"
           alt=""
         />
         <img
-          className="w-[200px] h-[170px] transition-transform duration-300 transform hover:scale-105"
+          className="w-[150px] h-[130px] xl:w-[200px] xl:h-[170px] transition-transform duration-300 transform hover:scale-105"
           src="https://stark-ver2.myshopify.com/cdn/shop/files/brand6.jpg?v=1693474083&width=200"
           alt=""
         />
       </div>
-      </>)
+    </div>
+  </>)
 }
 
 export default Brand
