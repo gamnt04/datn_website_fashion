e79008e2249777be836e13cc5b48c8fde8780ca2
BaoChung-Cloud <chungkbph38477@fpt.edu.vn>
2024-06-26 19:22:05 +0700

    6/25

--- BE/src/controllers/cart.js
+++ BE/src/controllers/cart.js
@@ -1,3 +1,41 @@
-import Cart from "../models/cart";
-import Product from "../models/";
+import Cart from "../models/cart.js";
+import Product from "../models/product.js";
 import User from "../models/cart";
+import { StatusCodes } from "http-status-codes";
+
+export const getCartByIdUser = async (req, res) => {
+  const { userId } = req.params;
+  try {
+    const cart = await Cart.findOne({ userId }).populate("products.productId");
+    return res.status(StatusCodes.OK).json({ cart });
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ error: "Internal Server Error" });
+  }
+};
+
+export const addProductsToCart = async (req, res) => {
+  const { userId, productId, quantity } = req.body;
+  try {
+    let cart = await Cart.findOne({ userId });
+    if (!cart) {
+      cart = new Cart({ userId, products: [] });
+    }
+    const existProductIndex = cart.products.findIndex(
+      (item) => item.productId.toString() === productId
+    );
+    if (existProductIndex !== -1) {
+      cart.products[existProductIndex].quantity += quantity;
+    } else {
+      cart.products.push({ productId, quantity });
+    }
+    await cart.save();
+    return res.status(StatusCodes.OK).json({ cart });
+  } catch (error) {
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ error: "Internal Server Error" });
+  }
+};
