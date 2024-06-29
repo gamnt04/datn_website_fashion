a6c4db70663db3f5f8a3c89f88663d9a2e6e8bd9
BaoChung-Cloud <chungkbph38477@fpt.edu.vn>
2024-06-29 19:08:59 +0700

    FE_add_to_cart

--- FE/src/common/hooks/Auth/useSignUp.tsx
+++ FE/src/common/hooks/Auth/useSignUp.tsx
@@ -1,29 +1,29 @@
-import { useMutation } from '@tanstack/react-query'
-import instance from '../../../configs/axios'
+import { useMutation } from "@tanstack/react-query";
+import instance from "../../../configs/axios";
 // import { useToast } from 'react-toastify'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from "react-router-dom";
 
 const useSignUp = () => {
-    // const { toast } = useToast()
-    const navigate = useNavigate()
-    const { mutate } = useMutation({
-        mutationFn: async (formData) => {
-            const { data } = await instance.post(`auth/signup`, formData)
-            return data
-        },
-        onSuccess: () => {
-            alert("Đăng ký thành công")
-            // toast({
-            //     title: "Đăng ký thành công",
-            //     variant: "success"
-            // })
-        },
-    })
-    const onSubmit = (formData: any) => {
-        mutate(formData);
-        navigate('/signin')
+  // const { toast } = useToast()
+  const navigate = useNavigate();
+  const { mutate } = useMutation({
+    mutationFn: async (formData) => {
+      const { data } = await instance.post(`auth/signup`, formData);
+      return data;
+    },
+    onSuccess: () => {
+      alert("Đăng ký thành công");
+      // toast({
+      //     title: "Đăng ký thành công",
+      //     variant: "success"
+      // })
     }
-    return { onSubmit }
-}
+  });
+  const onSubmit = (formData: any) => {
+    mutate(formData);
+    navigate("/login");
+  };
+  return { onSubmit };
+};
 
-export default useSignUp
+export default useSignUp;
