3d36871b6b4cf65e2fb99275064ac92e8f69b564
BaoChung-Cloud <chungkbph38477@fpt.edu.vn>
2024-06-30 20:39:08 +0700

    minicart-cart

--- FE/src/pages/Client/(Cart)/[MiniCart].tsx
+++ FE/src/pages/Client/(Cart)/[MiniCart].tsx
@@ -1,52 +1,40 @@
 import { Link } from "react-router-dom";
 import ScrollTop from "../../../common/hooks/Customers/ScrollTop";
 import { RecycleIcon } from "../../../resources/svg/Icon/Icon";
-<<<<<<< HEAD
 import { IProduct } from "../../../common/interfaces/Product";
 import { useCart } from "../../../common/hooks/Cart/useCart";
-import { ToastContainer } from "react-toastify";
 
 const MiniCart = () => {
   const {
     data,
     isLoading,
     isError,
-
     removeProductInCart,
     calculateTotal,
     calculateTotalProduct
   } = useCart();
+
   if (isLoading) return <p>Loading...</p>;
   if (isError) return <p>Error...</p>;
 
-  return (
-    <div className="absolute rounded bg-white shadow mb:w-[70vw] lg:w-[25vw] duration-300 right-0 group-hover:scale-100 scale-0 shadow-2xl p-4 group-hover:translate-y-[15px] lg:group-hover:translate-x-0 group-hover:translate-x-1/2 translate-x-1/2 -translate-y-1/2 before:absolute before:w-[65px] before:h-[40px] before:right-[-5px] before:top-0 before:translate-y-[-20px] before:bg-none">
-      {!data?.products || data?.products.length === 0 ? (
-        <div className="mb-[20px]">
-          <span className="text-sm">Please add products to your cart</span>
-=======
-
-const MiniCart = () => {
   return (
     <div
       className="absolute rounded bg-white shadow mb:w-[70vw] lg:w-[25vw] duration-300 right-0 group-hover:scale-100 scale-0 shadow-2xl 
     p-4 group-hover:translate-y-[15px] lg:group-hover:translate-x-0 group-hover:translate-x-1/2 translate-x-1/2 -translate-y-1/2
     before:absolute before:w-[65px] before:h-[40px] before:right-[-5px] before:top-0 before:translate-y-[-20px] before:bg-none"
     >
-      {/* có sản phần thì cho hiển thị ra */}
-      <div>
-        <div className="mb-[20px]">
-          <span className="text-sm">You have 3 items in you cart</span>
->>>>>>> e3a484af0d3c5f8542c09cec5df627e5948e56c3
+      {!data?.products || data?.products.length === 0 ? (
+        <div className="text-center p-4">
+          <p>Please add the product to your cart</p>
         </div>
       ) : (
         <>
+          <div className="mb-[20px]">
+            <span className="text-sm">
+              You have {calculateTotalProduct()} items in your cart
+            </span>
+          </div>
           <div className="grid h-full grid-flow-rows">
-            <div className="mb-[20px]">
-              <span className="text-sm">
-                You have {calculateTotalProduct()} items in you cart
-              </span>
-            </div>
             {data?.products.map((item: IProduct, index: number) => (
               <div
                 className="border-b w-full grid grid-cols-[70px_auto] py-[20px] gap-3 auto-rows-[70px]"
@@ -63,12 +51,10 @@ const MiniCart = () => {
                   <div className="flex flex-col *:text-sm justify-between">
                     <span>{item.name}</span>
                     <strong>
-                      {item.quantity} x $ {item.price}
+                      {item.quantity} x ${item.price}
                     </strong>
                     <span>Size: S</span>
                   </div>
-
-<<<<<<< HEAD
                   <div className="flex items-end cursor-pointer">
                     <button
                       type="button"
@@ -81,13 +67,11 @@ const MiniCart = () => {
               </div>
             ))}
           </div>
-
           <div>
             <div className="flex justify-between items-center my-6">
               <strong>Subtotal</strong>
               <strong>${calculateTotal()}</strong>
             </div>
-
             <div className="flex flex-col gap-y-[20px] *:rounded-md *:w-full *:h-[55px] *:grid *:place-items-center *:text-sm">
               <Link
                 onClick={ScrollTop}
@@ -100,49 +84,6 @@ const MiniCart = () => {
           </div>
         </>
       )}
-      {/* <ToastContainer /> */}
-=======
-        <div className="grid h-full grid-flow-rows">
-          <div className="border-b w-full grid grid-cols-[70px_auto] py-[20px] gap-3 auto-rows-[70px]">
-            <div className="bg-[#f2f2f2] p-2">
-              <img
-                className="relative z-[1] w-full h-full duration-300"
-                src="assets/Images/ethnic.png"
-                alt=""
-              />
-            </div>
-            <div className="w-full flex justify-between">
-              <div className="flex flex-col *:text-sm justify-between">
-                <span>San pham 1</span>
-                <strong>1 x $ 80.00</strong>
-                <span>Size: S</span>
-              </div>
-
-              <div className="flex items-end cursor-pointer">
-                <RecycleIcon />
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div>
-          <div className="flex justify-between items-center my-6">
-            <strong>Subtotal</strong>
-            <strong>$200.00</strong>
-          </div>
-
-          <div className="flex flex-col gap-y-[20px] *:rounded-md *:w-full *:h-[55px] *:grid *:place-items-center *:text-sm">
-            <Link
-              onClick={ScrollTop}
-              to={"/cart"}
-              className="bg-black text-white cursor-pointer"
-            >
-              View Cart
-            </Link>
-          </div>
-        </div>
-      </div>
->>>>>>> e3a484af0d3c5f8542c09cec5df627e5948e56c3
     </div>
   );
 };
