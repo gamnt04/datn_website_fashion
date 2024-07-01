022d79a0ac8d28b9458ef60e34aaea39356fea9e
BaoChung-Cloud <chungkbph38477@fpt.edu.vn>
2024-07-01 08:05:29 +0700

    fe-favoriteproduct

--- BE/src/routers/favoriteProducts.js
+++ BE/src/routers/favoriteProducts.js
@@ -9,7 +9,7 @@ const router = express.Router();
 //Lấy danh sách sản phẩm trong phần yêu thích
 router.get("/favorite/products/:userId", GetFavoriteProductByUserId);
 //thêm sản phầm vào danh mục yêu thích
-router.post("/favorite/products", addFavoriteProducts);
+router.post("/favorite/add-products", addFavoriteProducts);
 router.post("/favorite/remove-product", removeFavoriteProduct);
 
 export default router;
