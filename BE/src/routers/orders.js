3634c3712cec4ae36e62344ab25a2f8c0490120e
ducna34806 <ducnaph34806@fpt.edu.vn>
2024-07-07 22:05:04 +0700

    fix addPro

--- BE/src/routers/orders.js
+++ BE/src/routers/orders.js
@@ -1,11 +1,16 @@
 import { Router } from "express";
-import { createOrder, getOneOrderUser, getOrderById, getOrders, updateOrderStatus } from "../controllers/orders";
+import {
+  createOrder,
+  getOneOrderUser,
+  getOrderById,
+  getOrders,
+  updateOrderStatus,
+} from "../controllers/Orders/orders";
 
-const router = Router()
+const router = Router();
 router.post("/orders", createOrder);
 router.get("/orders", getOrders);
 router.get("/orders/:userId/:orderId", getOrderById);
 router.get("/orders/:userId", getOneOrderUser);
 router.patch("/orders/:userId/:orderId", updateOrderStatus);
-export default router
-
+export default router;
