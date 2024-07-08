9c25c38d591d2537746d6c11c9718b45c0f6f3d0
duonghainam1 <namhai0301004@gmail.com>
2024-07-08 15:45:38 +0700

    update pay html

--- FE/src/common/hooks/Cart/mutation_Carts.ts
+++ FE/src/common/hooks/Cart/mutation_Carts.ts
@@ -4,30 +4,30 @@ import { add_cart, dow_quantity, remove_quantity, up_quantity } from "../../../_
 
 type Actions = "ADD" | "UP" | "DOW" | "REMOVE";
 
-export function Mutation_Cart (action : Actions) {
+export function Mutation_Cart(action: Actions) {
     const queryClient = useQueryClient();
-    const {mutate, ...rest} = useMutation({
-        mutationFn : async (data : any) => {
+    const { mutate, ...rest } = useMutation({
+        mutationFn: async (data: any) => {
             switch (action) {
-                case "ADD" : 
-                return await add_cart(data);
-                case "UP" : 
-                return await up_quantity(data);
-                case "DOW" : 
-                return await dow_quantity(data);
-                case "REMOVE" : 
-                return await remove_quantity(data);
-                default : return
+                case "ADD":
+                    return await add_cart(data);
+                case "UP":
+                    return await up_quantity(data);
+                case "DOW":
+                    return await dow_quantity(data);
+                case "REMOVE":
+                    return await remove_quantity(data);
+                default: return
             }
         },
-        onSuccess :() => {
+        onSuccess: () => {
             queryClient.invalidateQueries({
-                queryKey : ['Cart_Key']
+                queryKey: ['Cart_Key']
             })
         },
-        onError : () => {
-            console.error ("Kiem tra lai server hoac internet!")
+        onError: () => {
+            console.error("Kiem tra lai server hoac internet!")
         }
     })
-    return {mutate, ...rest}
+    return { mutate, ...rest }
 }
--- FE/src/services/orderProduct.tsx
+++ FE/src/services/orderProduct.tsx
@@ -1,10 +1,61 @@
 import instance from "../configs/axios";
 
-export const GetAllOrder = async () => {
+export const GetAllOrder = async (page: number, status: string = "") => {
   try {
-    const { data } = await instance.get(`/orders`);
+    const { data } = await instance.get(`/orders?page=${page}&status=${status}`);
+    console.log(data);
+
     return data;
   } catch (error) {
     console.log(error);
   }
 };
+export const getOrderById = async (id: string) => {
+  try {
+    const { data } = await instance.get(`/orders/${id}`);
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+export const getOneOrderUser = async (userId: string) => {
+  try {
+    const { data } = await instance.post(`/orders/get_order_user`, { userId });
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const Add_Order = async (order: any) => {
+  try {
+    const { data } = await instance.post(`/orders`, order);
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+// export const Update_Status = async (id: string) => {
+//   const statusOrder: Record<string, string> = {
+//     "Chờ xác nhận": "Đang chuẩn bị hàng",
+//     "Đang chuẩn bị hàng": "Đang vận chuyển",
+//     "Đang vận chuyển": "Đã giao hàng",
+//   };
+//   try {
+//     const { data } = await instance.patch(`/orders/${id}`, { status: statusOrder });
+//     console.log(data);
+//     return data;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
+
+// router.post("/orders", createOrder);
+// router.get("/orders", getOrders);
+// router.post("/orders/get_order_user", getOneOrderUser);
+// router.get("/orders/:id", getOrderById);
+// router.patch("/orders/:id", updateOrderStatus);
