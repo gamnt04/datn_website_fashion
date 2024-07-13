a3a04e2324756bfb3389b721898d3edcc3540964
gamnt04 <gamsau2004@gmail.com>
2024-07-13 23:04:50 +0700

    be delete blog

--- BE/src/routers/blogs.js
+++ BE/src/routers/blogs.js
@@ -1,10 +1,11 @@
 import express from "express";
 import {
-GetAllBlogs, getBlogsById, createBlog,updateBlogById
+GetAllBlogs, getBlogsById, createBlog,updateBlogById, deleteBlogById
 } from "../controllers/Blog/blog.js";
 const Routes_blog = express.Router();
 Routes_blog.get("/blog/:blogId", getBlogsById);
 Routes_blog.get("/blogs", GetAllBlogs);
 Routes_blog.post("/blogs/add_blog", createBlog);
 Routes_blog.put("/blogs/:blogId", updateBlogById);
+Routes_blog.delete("/blogs/:blogId", deleteBlogById);
 export default Routes_blog;
