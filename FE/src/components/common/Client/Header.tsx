48e51296c9a9d93a48a3f293eab58c53ab39f60e
tulam120604 <lamctph35369@fpt.edu.vn>
2024-07-21 00:52:26 +0700

    fix layout item

--- FE/src/components/common/Client/Header.tsx
+++ FE/src/components/common/Client/Header.tsx
@@ -102,9 +102,9 @@ const Header = () => {
               viewBox="0 0 24 24"
               fill="none"
               stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               className="lucide lucide-menu"
             >
               <line x1="4" x2="20" y1="12" y2="12" />
@@ -211,7 +211,7 @@ const Header = () => {
             {/* option / menu */}
             <div className="cursor-pointer hover:scale-105 duration-300">
               {account && <Link to={'/allorder'}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-user-round-check"><path d="M2 21a8 8 0 0 1 13.292-6" /><circle cx="10" cy="8" r="5" /><path d="m16 19 2 2 4-4" /></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-user-round-check"><path d="M2 21a8 8 0 0 1 13.292-6" /><circle cx="10" cy="8" r="5" /><path d="m16 19 2 2 4-4" /></svg>
               </Link>}
               {account ? '' : <Link
                 to={"/login"}
