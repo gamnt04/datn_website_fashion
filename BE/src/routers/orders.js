7c13fa72c46c60c31d13e777f920cf1da682afa1
duonghainam1 <namhai0301004@gmail.com>
2024-07-21 21:10:38 +0700

    hủy đơn hàng có yêu cầu

--- BE/src/routers/orders.js
+++ BE/src/routers/orders.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import {
+  adminCancelOrder,
   createOrder,
   get_orders_client,
   getOneOrderUser,
   getOrderById,
 
-  updateOrderStatus
+  updateOrderStatus,
+  userCancelOrder
 } from "../controllers/Orders/orders";
 
 const router = Router();
@@ -14,4 +16,6 @@ router.get("/orders", get_orders_client);
 router.post("/orders/get_order_user", getOneOrderUser);
 router.get("/orders/:id", getOrderById);
 router.patch("/orders/:id", updateOrderStatus);
+router.post("/orders/:id/cancel/confirm", adminCancelOrder);
+router.post("/orders/:id/cancel", userCancelOrder);
 export default router;
