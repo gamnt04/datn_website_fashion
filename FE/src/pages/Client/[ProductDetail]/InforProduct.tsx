12531d51f40474cc0ff067757327addc4e35a978
tulam120604 <lamctph35369@fpt.edu.vn>
2024-07-21 14:48:04 +0700

    fix conflict

--- FE/src/pages/Client/[ProductDetail]/InforProduct.tsx
+++ FE/src/pages/Client/[ProductDetail]/InforProduct.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import React, { useState } from "react";
 import { Mutation_Cart } from "../../../common/hooks/Cart/mutation_Carts";
 import useLocalStorage from "../../../common/hooks/Storage/useStorage";
 import { IProduct } from "../../../common/interfaces/Product";
@@ -7,58 +6,32 @@ import { Button } from "../../../components/ui/button";
 import { Dow, Up } from "../../../resources/svg/Icon/Icon";
 import { Convert_Color } from "../../../_lib/Config/Config_Color";
 
-interface ImageProductProp {
+interface InforProductProp {
   product: IProduct;
 }
 const InforProduct: React.FC<InforProductProp> = ({ dataProps }: any) => {
   const [color, setColor] = useState();
-  const [arr_size, setArr_Size] = useState();
   const [size, setSize] = useState();
+  const [arr_size, setArr_Size] = useState();
   const [quantity_attr, setQuantity_attr] = useState();
+  const [quantity_item, setQuantity_item] = useState<number>(1);
   const dataItem = dataProps?.product;
   const dataAttr = dataProps?.attr;
   const { name_product, price_product, _id, stock } = dataItem;
   const [user] = useLocalStorage("user", {});
   const account = user?.user;
   const { mutate } = Mutation_Cart("ADD");
+
   const addCart = (id?: string | number) => {
-
-const InforProduct: React.FC<ImageProductProp> = ({ product }) => {
-  const { name_product, price_product, _id, quantity_product, sizes } = product;
-  const [user] = useLocalStorage("user", {});
-  const account = user?.user;
-  const { mutate } = Mutation_Cart("ADD");
-
-  const [quantity, setQuantity] = useState(1);
-  const [totalPrice, setTotalPrice] = useState(price_product);
-
-  const handleIncreaseQuantity = () => {
-    if (quantity < quantity_product) {
-      const newQuantity = quantity + 1;
-      setQuantity(newQuantity);
-      setTotalPrice(newQuantity * price_product);
-    }
-  };
-
-  const handleDecreaseQuantity = () => {
-    if (quantity > 1) {
-      const newQuantity = quantity - 1;
-      setQuantity(newQuantity);
-      setTotalPrice(newQuantity * price_product);
-    }
-  };
-
-  const addCart = (id: string) => {
     const item = {
       userId: account,
       productId: id,
-      quantity: quantity
+      quantity: 1
     };
     mutate(item);
   };
-
   useEffect(() => {
-    if(!dataAttr) {
+    if (!dataAttr) {
       setQuantity_attr(stock)
     }
   }, [dataAttr])
@@ -90,6 +63,23 @@ const InforProduct: React.FC<ImageProductProp> = ({ product }) => {
       default: return;
     }
   }
+  function handle_quantity_item(action : any) {
+    switch (action) {
+      case 'dow':
+        if (quantity_item > 1) {
+          setQuantity_item(quantity_item - 1)
+        }
+        return
+      case 'up':
+        if (quantity_item < quantity_attr) {
+          setQuantity_item(quantity_item + 1)
+        } else {
+          alert('Vượt quá số lượng sản phẩm!')
+        }
+        return
+    }
+
+  }
 
   return (
     <div className="h-full w-full *:w-full lg:mt-2 mb:mt-5">
@@ -102,6 +92,13 @@ const InforProduct: React.FC<ImageProductProp> = ({ product }) => {
           <strong className="lg:text-2xl lg:mt-0 mb:mt-3.5 mb:text-xl lg:tracking-[-1.2px] font-medium lg:leading-[38.4px]"></strong>
           <div className="flex flex-col gap-y-2 justify-between">
             <section className="lg:w-[163px] mb:w-[157px] mb:mt-[8px] lg:mt-0 h-[21px] *:lg:text-sm *:mb:text-xs flex justify-between items-start">
+              {/* neu co danh gia bang sao thi bo vao */}
+              {/* <div className="flex items-start lg:gap-x-0 mb:gap-x-1">
+                                <svg xmlns="http://www.w3.org/2000/svg" width={16} height={16} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-star">
+                                    <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2" />
+                                </svg>
+                                <strong>4.6/5</strong>
+                            </div> */}
               <div className="flex gap-x-2">
                 <strong>135</strong>
                 <span className="text-[#C8C9CB]">Reviews</span>
@@ -112,7 +109,7 @@ const InforProduct: React.FC<ImageProductProp> = ({ product }) => {
                 <del className="font-light lg:text-sm mb:text-sm text-[#9D9EA2]">
                   200.00 đ
                 </del>
-                {totalPrice} đ
+                {price_product}
               </span>
             </div>
           </div>
@@ -151,39 +148,6 @@ const InforProduct: React.FC<ImageProductProp> = ({ product }) => {
             }
           </>)
         }
-
-        <div className="flex flex-col gap-y-1 lg:mt-[2px] mt-[3px] lg:pb-0 mb:pb-[21px] font-medium">
-          <span className="text-sm lg:text-base tracking-[1px]">
-            Mô tả ngắn
-          </span>
-          <p className="text-sm lg:text-base text-gray-600">
-            Ahihi day la mo ta ngan cua san pham demo ahihihihihihihihihihih
-          </p>
-        </div>
-        {/* row 3 */}
-        <div>
-          <span className="text-lg lg:mt-[1px] mb:mt-3.5 lg:tracking-[-1.2px] font-medium lg:leading-[38.4px]">
-            Size
-          </span>
-          <div className="flex items-center gap-x-4 lg:mt-[2px] mt-[3px] lg:pb-0 mb:pb-[21px] font-medium *:px-4 *:py-2 *:rounded *:border *:duration-300">
-            {sizes?.map((size, index) => (
-              <button className="hover:bg-black hover:text-white" key={index}>
-                {size}
-              </button>
-            ))}
-          </div>
-        </div>
-        {/* row 4 */}
-        <div>
-          <span className="text-lg lg:mt-[1px] mb:mt-3.5 lg:tracking-[-1.2px] font-medium lg:leading-[38.4px]">
-            Color
-          </span>
-          <div className="flex items-center gap-x-4 lg:mt-[2px] mt-[3px] lg:pb-0 mb:pb-[21px] font-medium *:h-8 *:w-8 *:rounded-[50%] *:relative *:border *:duration-300">
-            <button className="bg-red-500 hover:scale-110 after:absolute focus:after:w-4 focus:after:h-2 focus:after:border-l-2 focus:after:border-b-2 after:border-white after:rotate-[-45deg] grid place-items-center"></button>
-            <button className="bg-blue-500 hover:scale-110 after:absolute after:border-0 focus:after:w-4 focus:after:h-2 focus:after:border-l-2 focus:after:border-b-2 after:border-white after:rotate-[-45deg] grid place-items-center"></button>
-            <button className="bg-black hover:scale-110 after:absolute after:border-0 focus:after:w-4 focus:after:h-2 focus:after:border-l-2 focus:after:border-b-2 after:border-white after:rotate-[-45deg] grid place-items-center"></button>
-          </div>
-        </div>
         {/* row 5 */}
         <div className="py-5 *:w-full rounded-xl lg:-mt-5 -mt-1">
           {/* quantity */}
@@ -191,48 +155,12 @@ const InforProduct: React.FC<ImageProductProp> = ({ product }) => {
             {/* up , dow quantity */}
             <div className="border lg:py-2.5 lg:pr-6  mb:py-1 mb:pl-2 mb:pr-[18px] *:text-xs flex flex items-center gap-x-3 rounded-xl">
               <div className="flex items-center *:w-9 *:h-9 gap-x-1 *:grid *:place-items-center">
-                <button>
+                <button onClick={() => handle_quantity_item('dow')}>
                   <Dow />
                 </button>
-                <input className="bg-[#F4F4F4] text-center rounded" value={2} />
-                <button>
+                <input className="bg-[#F4F4F4] text-center rounded" value={quantity_item} />
+                <button onClick={() => handle_quantity_item('up')}>
                   <Up />
-            {/* up, down quantity */}
-            <div className="border lg:py-2.5 lg:pr-6  mb:py-1 mb:pl-2 mb:pr-[18px] *:text-xs flex items-center gap-x-3 rounded-xl">
-              <div className="flex items-center *:w-9 *:h-9 gap-x-1 *:grid *:place-items-center">
-                <button onClick={handleDecreaseQuantity}>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width={12}
-                    height={12}
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth={2}
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    className="lucide lucide-minus"
-                  >
-                    <path d="M5 12h14" />
-                  </svg>
-                </button>
-                <input className="bg-[#F4F4F4]" value={quantity} readOnly />
-                <button onClick={handleIncreaseQuantity}>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width={12}
-                    height={12}
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth={2}
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    className="lucide lucide-plus"
-                  >
-                    <path d="M5 12h14" />
-                    <path d="M12 5v14" />
-                  </svg>
                 </button>
               </div>
               <span className="text-gray-800 lg:tracking-[0.5px] border-l pl-4 border-black">
@@ -244,8 +172,6 @@ const InforProduct: React.FC<ImageProductProp> = ({ product }) => {
             <span>Tạm tính :</span>
             <span className="text-[#EB2606]">
               242.00 đ
-            <span className="font-medium text-[#EB2606] lg:text-xl lg:tracking-[0.7px] mb:text-base flex items-center lg:gap-x-3 lg:mt-0.5 mb:gap-x-2">
-              {totalPrice} đ
             </span>
           </div>
 
