97c241db5c07aa47891a55cedddf675d05608354
tulam120604 <lamctph35369@fpt.edu.vn>
2024-07-26 01:48:52 +0700

    26/7

--- FE/src/common/hooks/Pay/mutation_Pay.tsx
+++ FE/src/common/hooks/Pay/mutation_Pay.tsx
@@ -1,16 +1,11 @@
 import { useMutation } from "@tanstack/react-query";
 import { Add_Order } from "../../../services/orderProduct";
 import useLocalStorage from "../Storage/useStorage";
-import { List_Cart } from "../Cart/querry_Cart";
-import { reduce } from "lodash";
 import { useNavigate } from "react-router-dom";
 
 export function Pay_Mutation() {
     const [user] = useLocalStorage("user", {})
     const userId = user?.user?._id
-    const { data } = List_Cart(userId);
-    console.log(data);
-
     const navigate = useNavigate()
     const { mutate } = useMutation({
         mutationFn: async (order: {
@@ -30,26 +25,9 @@ export function Pay_Mutation() {
         onError: () => {
             alert("Đặt hàng thất bại")
         }
-
-
     })
-    console.log(data);
-
-    // console.log(order);
-    // const calcuateTotal = () => {
-    //     if (!data || !data.products) return 0
-    //     return reduce(data.products, (total: any, product: any) => total + product.price * product.quantity, 0)
-    // }
-    const onSubmit = (formData: object) => {
-        mutate({
-            userId,
-            items: data,
-            totalPrice: data?.total_price,
-            customerInfo: formData,
-
-        });
-
-
+    const onSubmit = (formData: any) => {
+        mutate(formData);
     };
-    return { mutate, onSubmit, data }
+    return { mutate, onSubmit, userId }
 }
