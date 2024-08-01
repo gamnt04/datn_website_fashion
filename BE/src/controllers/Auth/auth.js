95bead47b804cc73bfcaa0f4fb5c52d257b79d5c
gamnt04 <gamsau2004@gmail.com>
2024-08-02 00:21:06 +0700

    fix signup

--- BE/src/controllers/Auth/auth.js
+++ BE/src/controllers/Auth/auth.js
@@ -17,28 +17,26 @@ export const GetAllUser = async (req, res) => {
   }
 };
 export const Get_All_User_Search = async (req, res) => {
-  const {
-    _search = ''
-  } = req.query
+  const { _search = "" } = req.query;
   try {
     const querry = {};
     if (_search) {
       querry.$and = [
         {
-          userName: { $regex: new RegExp(_search, 'i') }
-        }
-      ]
+          userName: { $regex: new RegExp(_search, "i") },
+        },
+      ];
     }
     const user = await User.find(querry);
     console.log(user);
     return res.status(StatusCodes.OK).json({
       message: "Done !",
-      user
+      user,
     });
   } catch (error) {
     console.error("Error getting all products:", error);
     return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-      message: error.message || "Loi server !"
+      message: error.message || "Loi server !",
     });
   }
 };
@@ -74,32 +72,37 @@ const generateAccessToken = (userId) => {
 export const signup = async (req, res) => {
   const { email, password } = req.body;
   const { error } = signupSchema.validate(req.body, { abortEarly: false });
-  if (error) {
-    const messages = error.details.map((item) => item.message);
-    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-      messages,
-    });
-  }
-
   const existUser = await User.findOne({ email });
-  if (existUser) {
-    return res.status(StatusCodes.BAD_REQUEST).json({
-      messages: ["Email đã tồn tại"],
-    });
-  }
-  // Mã hóa mật khẩu
-  const hashedPassword = await bcryptjs.hash(password, 10);
-  // Nếu không có user nào trong hệ thống thì tạo user đầu tiên là admin
-  const role = (await User.countDocuments({})) === 0 ? "admin" : "user";
 
-  const user = await User.create({
-    ...req.body,
-    password: hashedPassword,
-    role,
-  });
-  return res
-    .status(StatusCodes.CREATED)
-    .json({ message: "Đăng ký tài khoản thành công", user });
+  try {
+    if (error) {
+      const messages = error.details.map((item) => item.message);
+      return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+        messages,
+      });
+    }
+
+    if (existUser) {
+      return res.status(StatusCodes.BAD_REQUEST).json({
+        messages: ["Email đã tồn tại"],
+      });
+    }
+    // Mã hóa mật khẩu
+    const hashedPassword = await bcryptjs.hash(password, 10);
+    // Nếu không có user nào trong hệ thống thì tạo user đầu tiên là admin
+    const role = (await User.countDocuments({})) === 0 ? "admin" : "user";
+
+    const user = await User.create({
+      ...req.body,
+      password: hashedPassword,
+      role,
+    });
+    return res
+      .status(StatusCodes.CREATED)
+      .json({ message: "Đăng ký tài khoản thành công", user });
+  } catch (error) {
+    console.error(`Đăng ký thất bại do:`, error);
+  }
 };
 export const signin = async (req, res) => {
   const { email, password } = req.body;
