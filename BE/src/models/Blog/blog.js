33abbd745c3834252fa0b70eecbf270f32f6e15d
Minhcoderdao <minhtoyotabla@gmail.com>
2024-08-11 05:08:59 +0700

    fix Blog

--- BE/src/models/Blog/blog.js
+++ BE/src/models/Blog/blog.js
@@ -5,7 +5,10 @@ const blogSchema = new Schema({
     // title: { type: String, required: true },
     content: { type: String, required: true },
     author: { type: String, required: true },
-    // createdAt: { type: Date, default: Date.now },
+    // createdAt: { type: Date, default: Date.now } uh huhu,
+    // createdAt: { type: Date, default: Date.now } uh huhu,
+    // createdAt: { type: Date, default: Date.now } uh huhu,
+    // createdAt: { type: Date, default: Date.now } uh huhu,
     published: { type: Boolean, default: false },
     // imageUrl: { type: String},
 },{timestamps: true, versionKey: false});
