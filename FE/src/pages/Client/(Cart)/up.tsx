55f79d8fc6b44ae034f282c90bd8b592364d8f76
tulam120604 <saudoi1204@gmail.com>
2024-08-14 16:20:51 +0700

    xong 90% cong viec

--- FE/src/pages/Client/(Cart)/up.tsx
+++ FE/src/pages/Client/(Cart)/up.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Mutation_Cart } from '../../../common/hooks/Cart/mutation_Carts';
 import useLocalStorage from '../../../common/hooks/Storage/useStorage';
 import { Button } from 'antd';
-
+import {toast} from 'react-toastify'
 const Up_btn = ({ dataProps }: any) => {
     const [user] = useLocalStorage("user", {});
     const account = user?.user;
@@ -11,10 +11,14 @@ const Up_btn = ({ dataProps }: any) => {
         const data = {
             userId: account,
             productId: dataProps?.id_item,
-            color : dataProps?.color,
-            size : dataProps?.size
+            color: dataProps?.color,
+            size: dataProps?.size
         }
-        mutate(data)
+        if (dataProps?.quantity_item?.quantity >= dataProps?.quantity_item?.quantity_attr) {
+            toast.error('Vượt quá số lượng sản phẩm!', {autoClose : 500})
+        } else {
+            mutate(data)
+        }
     }
 
     return (
