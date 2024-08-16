5d15f3831127b37708fe523afd9bf33f0ab3fee1
gamnt2004 <gamsau2004@gmail.com>
2024-08-17 00:41:39 +0700

    fix address loi pha

--- FE/src/components/common/Client/_component/Address.tsx
+++ FE/src/components/common/Client/_component/Address.tsx
@@ -17,6 +17,7 @@ import {
 import instance from "../../../../configs/axios";
 import useLocalStorage from "../../../../common/hooks/Storage/useStorage";
 import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 type FieldType = {
   userId: string;
   fullName: string;
@@ -26,6 +27,258 @@ type FieldType = {
   checked: boolean;
   newAddress: string;
 };
+// export const Add_Address = ({ handleAddress }: any) => {
+//   const [user] = useLocalStorage("user", {});
+//   const userId = user?.user?._id;
+//   const querryClient = useQueryClient();
+//   const [form] = Form.useForm();
+//   const [messageApi, contextHolder] = message.useMessage();
+
+//   const [provinces, setProvinces] = useState([]);
+//   const [districts, setDistricts] = useState([]);
+//   const [wards, setWards] = useState([]);
+//   const [villages, setVillages] = useState([]);
+
+//   const [selectedProvince, setSelectedProvince] = useState(null);
+//   const [selectedDistrict, setSelectedDistrict] = useState(null);
+//   const [selectedWard, setSelectedWard] = useState(null);
+
+//   useEffect(() => {
+//     async function fetchData() {
+//       // Fetch data for provinces
+//       const provincesData = await instance.get("/api/provinces");
+//       setProvinces(provincesData.data);
+
+//       // Initially empty districts, wards, and villages
+//       setDistricts([]);
+//       setWards([]);
+//       setVillages([]);
+//     }
+
+//     fetchData();
+//   }, []);
+
+//   const handleProvinceChange = async (provinceId: string) => {
+//     setSelectedProvince(provinceId);
+//     setSelectedDistrict(null);
+//     setSelectedWard(null);
+
+//     // Fetch districts based on selected province
+//     const districtsData = await instance.get(
+//       `/api/districts?province=${provinceId}`
+//     );
+//     setDistricts(districtsData.data);
+
+//     // Reset wards and villages
+//     setWards([]);
+//     setVillages([]);
+//   };
+
+//   const handleDistrictChange = async (districtId: string) => {
+//     setSelectedDistrict(districtId);
+//     setSelectedWard(null);
+
+//     // Fetch wards based on selected district
+//     const wardsData = await instance.get(`/api/wards?district=${districtId}`);
+//     setWards(wardsData.data);
+
+//     // Reset villages
+//     setVillages([]);
+//   };
+
+//   const handleWardChange = async (wardId: string) => {
+//     setSelectedWard(wardId);
+
+//     // Fetch villages based on selected ward
+//     const villagesData = await instance.get(`/api/villages?ward=${wardId}`);
+//     setVillages(villagesData.data);
+//   };
+
+//   const { mutate } = useMutation({
+//     mutationFn: async (formData) => {
+//       const { data } = await instance.post(`/auth/add_address`, formData);
+//       form.resetFields();
+//       return data;
+//     },
+//     onSuccess: () => {
+//       querryClient.invalidateQueries({
+//         queryKey: ["AUTH_KEY"],
+//       });
+//       messageApi.open({
+//         type: "success",
+//         content: "Thêm địa chỉ thành công",
+//       });
+//     },
+//     onError: () => {
+//       messageApi.open({
+//         type: "error",
+//         content: "Thêm địa chỉ thất bại!",
+//       });
+//     },
+//   });
+
+//   const onFinish: FormProps<FieldType>["onFinish"] = (values) => {
+//     const data_form: any = {
+//       userId: userId,
+//       newAddress: values,
+//       setDefault: values.checked, // Gửi thông tin về việc thiết lập địa chỉ làm mặc định
+//     };
+//     mutate(data_form);
+//   };
+
+//   return (
+//     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 backdrop-blur-sm z-50">
+//       <div className="bg-white p-5 border rounded relative w-[400px] lg:w-[500px]">
+//         <h1 className="py-3 text-center font-medium">Địa chỉ mới</h1>
+//         {contextHolder}
+//         <Form
+//           form={form}
+//           name="basic"
+//           initialValues={{ remember: true }}
+//           onFinish={onFinish}
+//           autoComplete="off"
+//         >
+//           <Form.Item
+//             name="fullName"
+//             className="w-full my-3"
+//             rules={[
+//               { required: true, message: "Vui lòng nhập họ và tên!" },
+//               { min: 3, message: "Tên phải có ít nhất 3 ký tự!" },
+//             ]}
+//           >
+//             <Input
+//               className="w-full border px-2 py-2 rounded focus:ring-0"
+//               placeholder="Họ và tên"
+//             />
+//           </Form.Item>
+
+//           <Form.Item
+//             name="phoneNumber"
+//             className="w-full my-3"
+//             rules={[
+//               { required: true, message: "Vui lòng nhập số điện thoại!" },
+//               {
+//                 pattern: /^[0-9]{10,11}$/,
+//                 message: "Số điện thoại không hợp lệ!",
+//               },
+//             ]}
+//           >
+//             <Input
+//               className="w-full border px-2 py-2 rounded focus:ring-0"
+//               placeholder="Số điện thoại"
+//             />
+//           </Form.Item>
+//           <Form.Item
+//             name="province"
+//             className="w-full my-3"
+//             rules={[{ required: true, message: "Vui lòng chọn thành phố!" }]}
+//           >
+//             <Select
+//               placeholder="Chọn thành phố"
+//               onChange={handleProvinceChange}
+//               className="w-full"
+//             >
+//               {provinces.map((province) => (
+//                 <Option key={province.id} value={province.id}>
+//                   {province.name}
+//                 </Option>
+//               ))}
+//             </Select>
+//           </Form.Item>
+
+//           <Form.Item
+//             name="district"
+//             className="w-full my-3"
+//             rules={[{ required: true, message: "Vui lòng chọn huyện!" }]}
+//           >
+//             <Select
+//               placeholder="Chọn huyện"
+//               onChange={handleDistrictChange}
+//               disabled={!selectedProvince}
+//               className="w-full"
+//             >
+//               {districts.map((district) => (
+//                 <Option key={district.id} value={district.id}>
+//                   {district.name}
+//                 </Option>
+//               ))}
+//             </Select>
+//           </Form.Item>
+
+//           <Form.Item
+//             name="ward"
+//             className="w-full my-3"
+//             rules={[{ required: true, message: "Vui lòng chọn xã!" }]}
+//           >
+//             <Select
+//               placeholder="Chọn xã"
+//               onChange={handleWardChange}
+//               disabled={!selectedDistrict}
+//               className="w-full"
+//             >
+//               {wards.map((ward) => (
+//                 <Option key={ward.id} value={ward.id}>
+//                   {ward.name}
+//                 </Option>
+//               ))}
+//             </Select>
+//           </Form.Item>
+
+//           <Form.Item
+//             name="village"
+//             className="w-full my-3"
+//             rules={[{ required: true, message: "Vui lòng chọn thôn!" }]}
+//           >
+//             <Select
+//               placeholder="Chọn thôn"
+//               disabled={!selectedWard}
+//               className="w-full"
+//             >
+//               {villages.map((village) => (
+//                 <Option key={village.id} value={village.id}>
+//                   {village.name}
+//                 </Option>
+//               ))}
+//             </Select>
+//           </Form.Item>
+
+//           <Form.Item
+//             name="addressDetails"
+//             className="w-full my-3"
+//             rules={[
+//               { required: true, message: "Vui lòng nhập địa chỉ cụ thể!" },
+//             ]}
+//           >
+//             <Input
+//               className="w-full border px-2 py-2 rounded focus:ring-0"
+//               placeholder="Địa chỉ cụ thể"
+//             />
+//           </Form.Item>
+
+//           <Form.Item
+//             className="w-full my-3 flex items-center gap-3"
+//             name="checked"
+//             valuePropName="checked"
+//           >
+//             <Checkbox>Đặt làm mặc định</Checkbox>
+//           </Form.Item>
+
+//           <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+//             <Button htmlType="submit" className="h-10 bg-black text-white">
+//               Hoàn Thành
+//             </Button>
+//           </Form.Item>
+//         </Form>
+//         <Button
+//           onClick={handleAddress}
+//           className="hover:bg-slate-100 hover:rounded-full hover:border-2 w-8 h-8 border-0 absolute top-5 right-5 rounded px-2 py-2"
+//         >
+//           <CloseOutlined />
+//         </Button>
+//       </div>
+//     </div>
+//   );
+// };
 
 export const Add_Address = ({ handleAddress }: any) => {
   const [user] = useLocalStorage("user", {});
@@ -33,6 +286,7 @@ export const Add_Address = ({ handleAddress }: any) => {
   const querryClient = useQueryClient();
   const [form] = Form.useForm();
   const [messageApi, contextHolder] = message.useMessage();
+
   const { mutate } = useMutation({
     mutationFn: async (formData) => {
       const { data } = await instance.post(`/auth/add_address`, formData);
@@ -55,13 +309,16 @@ export const Add_Address = ({ handleAddress }: any) => {
       });
     },
   });
+
   const onFinish: FormProps<FieldType>["onFinish"] = (values) => {
     const data_form: any = {
       userId: userId,
       newAddress: values,
+      setDefault: values.checked, // Gửi thông tin về việc thiết lập địa chỉ làm mặc định
     };
     mutate(data_form);
   };
+
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 backdrop-blur-sm z-50">
       <div className="bg-white p-5 border rounded relative w-[400px] lg:w-[500px]">
@@ -78,7 +335,8 @@ export const Add_Address = ({ handleAddress }: any) => {
             name="fullName"
             className="w-full my-3"
             rules={[
-              { required: true, message: "Please input your full name!" },
+              { required: true, message: "Vui lòng nhập họ và tên!" },
+              { min: 3, message: "Tên phải có ít nhất 3 ký tự!" },
             ]}
           >
             <Input
@@ -91,7 +349,11 @@ export const Add_Address = ({ handleAddress }: any) => {
             name="phoneNumber"
             className="w-full my-3"
             rules={[
-              { required: true, message: "Please input your phone number!" },
+              { required: true, message: "Vui lòng nhập số điện thoại!" },
+              {
+                pattern: /^[0-9]{10,11}$/,
+                message: "Số điện thoại không hợp lệ!",
+              },
             ]}
           >
             <Input
@@ -103,9 +365,7 @@ export const Add_Address = ({ handleAddress }: any) => {
           <Form.Item
             name="address"
             className="w-full my-3"
-            rules={[
-              { required: true, message: "Please input your address details!" },
-            ]}
+            rules={[{ required: true, message: "Vui lòng nhập địa chỉ!" }]}
           >
             <Input
               className="w-full border px-2 py-2 rounded focus:ring-0"
@@ -113,16 +373,7 @@ export const Add_Address = ({ handleAddress }: any) => {
             />
           </Form.Item>
 
-          <Form.Item
-            name="addressDetails"
-            className="w-full my-3"
-            rules={[
-              {
-                required: true,
-                message: "Please input your specific address!",
-              },
-            ]}
-          >
+          <Form.Item name="addressDetails" className="w-full my-3">
             <Input
               className="w-full border px-2 py-2 rounded focus:ring-0"
               placeholder="Địa chỉ cụ thể"
@@ -154,29 +405,46 @@ export const Add_Address = ({ handleAddress }: any) => {
   );
 };
 
-export const Update_Address = ({ handleUpdateAddress }: any) => {
+export const Update_Address = ({ addressId, setIsOpenUpdate }: any) => {
   const [user] = useLocalStorage("user", {});
   const userId = user?.user?._id;
-  const queryClient = useQueryClient();
+  console.log(userId);
+  console.log(addressId);
+
   const [form] = Form.useForm();
   const [messageApi, contextHolder] = message.useMessage();
+  const queryClient = useQueryClient();
+  const closeOpenUpdate = () => {
+    setIsOpenUpdate(false);
+  };
 
   // Lấy thông tin địa chỉ người dùng
-  const { data } = useQuery({
-    queryKey: ["AUTH_KEY", userId],
+  const { data, isSuccess } = useQuery({
+    queryKey: ["AUTH_KEY", addressId],
     queryFn: async () => {
-      const { data } = await instance.get(`/auth/address/${userId}`);
+      const { data } = await instance.get(
+        `/auth/address/${userId}/${addressId}`
+      );
       return data;
     },
   });
+  console.log(data?.address);
+
+  // Cập nhật giá trị của form khi dữ liệu đã được tải
+  useEffect(() => {
+    if (isSuccess && data?.address) {
+      form.setFieldsValue(data.address);
+    }
+  }, [isSuccess, data, form]);
 
   // Mutation để cập nhật địa chỉ
   const { mutate } = useMutation({
     mutationFn: async (formData) => {
-      const { data } = await instance.put(`/auth/address/${userId}`, {
-        updatedAddress: formData,
-      });
-      form.resetFields();
+      const { data } = await instance.put(
+        `/auth/${userId}/${addressId}`,
+        formData
+      );
+      // form.resetFields();
       return data;
     },
     onSuccess: () => {
@@ -203,12 +471,12 @@ export const Update_Address = ({ handleUpdateAddress }: any) => {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 backdrop-blur-sm z-50">
       <div className="bg-white p-5 border rounded relative w-[400px] lg:w-[500px]">
-        <h1 className="py-3 text-center font-medium">Địa chỉ mới</h1>
+        <h1 className="py-3 text-center font-medium">Chỉnh sửa địa chỉ</h1>
         {contextHolder}
         <Form
           form={form}
           name="basic"
-          initialValues={{ ...data?.address }}
+          // initialValues={{ ...data?.address }}
           onFinish={onFinish}
           autoComplete="off"
         >
@@ -216,7 +484,7 @@ export const Update_Address = ({ handleUpdateAddress }: any) => {
             name="fullName"
             className="w-full my-3"
             rules={[
-              { required: true, message: "Please input your full name!" },
+              { required: true, message: "Họ và Tên không được để trống!" },
             ]}
           >
             <Input
@@ -229,7 +497,11 @@ export const Update_Address = ({ handleUpdateAddress }: any) => {
             name="phoneNumber"
             className="w-full my-3"
             rules={[
-              { required: true, message: "Please input your phone number!" },
+              { required: true, message: "Vui lòng nhập số điện thoại!" },
+              {
+                pattern: /^[0-9]{10,11}$/,
+                message: "Số điện thoại không hợp lệ!",
+              },
             ]}
           >
             <Input
@@ -242,7 +514,7 @@ export const Update_Address = ({ handleUpdateAddress }: any) => {
             name="address"
             className="w-full my-3"
             rules={[
-              { required: true, message: "Please input your address details!" },
+              { required: true, message: "Địa chỉ không được để trống!" },
             ]}
           >
             <Input
@@ -251,16 +523,7 @@ export const Update_Address = ({ handleUpdateAddress }: any) => {
             />
           </Form.Item>
 
-          <Form.Item
-            name="addressDetails"
-            className="w-full my-3"
-            rules={[
-              {
-                required: true,
-                message: "Please input your specific address!",
-              },
-            ]}
-          >
+          <Form.Item name="addressDetails" className="w-full my-3">
             <Input
               className="w-full border px-2 py-2 rounded focus:ring-0"
               placeholder="Địa chỉ cụ thể"
@@ -274,7 +537,7 @@ export const Update_Address = ({ handleUpdateAddress }: any) => {
           </Form.Item>
         </Form>
         <Button
-          onClick={handleUpdateAddress}
+          onClick={closeOpenUpdate}
           className="hover:bg-slate-100 hover:rounded-full hover:border-2 w-8 h-8 border-0 absolute top-5 right-5 rounded px-2 py-2"
         >
           <CloseOutlined />
@@ -304,10 +567,7 @@ export const List_Address = ({
                 <PlusOutlined />
               </Button>
             </div>
-            <div
-              className="flex justify-between items-center my-5 border-b pb-6"
-
-            >
+            <div className="flex justify-between items-center my-5 border-b pb-6">
               <div className="py-1 flex items-start gap-4">
                 <div>
                   <h1>
@@ -317,7 +577,6 @@ export const List_Address = ({
                   </h1>
                   <p className="text-gray-400 py-2">{auth?.addressDetails}</p>
                   <p className="text-gray-400">{auth?.address}</p>
-
                 </div>
               </div>
               <div className="">
--- FE/src/pages/Client/Profile/Address.tsx
+++ FE/src/pages/Client/Profile/Address.tsx
@@ -1,137 +1,158 @@
-import { useState } from "react";
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import instance from "../../../configs/axios";
-import { Button, message, Popconfirm } from "antd";
+import { Auth } from "../../../common/interfaces/Auth";
 import useLocalStorage from "../../../common/hooks/Storage/useStorage";
+import { List_Auth } from "../../../common/hooks/Auth/querry_Auth";
 import ProfileHook from "../../../common/hooks/Settings/ProfileHook";
 import {
   Add_Address,
   Update_Address,
 } from "../../../components/common/Client/_component/Address";
+import { useState } from "react";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import instance from "../../../configs/axios";
+import { Button, message, Popconfirm } from "antd";
 
 const Address = () => {
-  const { isLoading, isError, error } = ProfileHook();
+  const { isLoading, isPending, isError, error } = ProfileHook();
   const queryClient = useQueryClient();
   const [user] = useLocalStorage("user", {});
   const userId = user?.user?._id;
+  const { data, refetch } = List_Auth(userId);
+  const [isOpen, setIsOpen] = useState(false);
+  const [isOpenUpdate, setIsOpenUpdate] = useState(false);
+  const [addressId, setAddressId] = useState<string | null>(null);
 
-  const { data } = useQuery({
-    queryKey: ["AUTH_KEY", userId],
-    queryFn: async () => {
-      const { data } = await instance.get(`/auth/address/${userId}`);
+  const handleUpdateAddress = (id: string) => {
+    setAddressId(id);
+    setIsOpenUpdate(true);
+  };
+
+  const handleAddress = () => {
+    setIsOpen(!isOpen);
+    if (addressId) setAddressId(null);
+  };
+
+  const { mutate } = useMutation({
+    mutationFn: async (id: string) => {
+      const { data } = await instance.delete(`/auth/${userId}/${id}`);
       return data;
-    },
-  });
-  console.log(data?.address);
-
-  const { mutate: deleteAddress } = useMutation({
-    mutationFn: async () => {
-      await instance.delete(`/auth/address/${userId}`);
     },
     onSuccess: () => {
       queryClient.invalidateQueries({
         queryKey: ["AUTH_KEY"],
       });
-      message.open({
-        type: "success",
-        content: "Xóa địa chỉ thành công",
-      });
+      message.success("Xóa địa chỉ thành công");
     },
     onError: () => {
-      message.open({
-        type: "error",
-        content: "Xóa địa chỉ thất bại!",
-      });
+      message.error("Xóa địa chỉ thất bại");
     },
   });
 
-  const [isOpen, setIsOpen] = useState(false);
-  const [isOpenUpdate, setIsOpenUpdate] = useState(false);
-
-  const handleAddress = () => {
-    setIsOpen(!isOpen);
+  const handleDeleteAddress = (id: string, isDefault: boolean) => {
+    if (isDefault) {
+      message.error("Không thể xóa địa chỉ mặc định");
+      return;
+    }
+    mutate(id);
   };
 
-  const handleUpdateAddress = () => {
-    setIsOpenUpdate(!isOpenUpdate);
-  };
+  const { mutate: setDefaultAddress } = useMutation({
+    mutationFn: async (addressId: string) => {
+      await instance.patch(`/auth/${userId}/${addressId}/default`);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["AUTH_KEY"] });
+      message.success("Đã thiết lập địa chỉ mặc định thành công");
+    },
+    onError: () => {
+      message.error("Thiết lập địa chỉ mặc định thất bại");
+    },
+  });
 
   if (isLoading) return <div>Loading...</div>;
+  if (isPending) return <div>Pending...</div>;
   if (isError) return <div>{error.message}</div>;
+
+  // Sắp xếp địa chỉ mặc định lên đầu tiên
+  const sortedAddresses = data?.address?.sort((a, b) => b.checked - a.checked);
 
   return (
     <>
       <div>
         <div className="flex items-center justify-between px-5 py-4 border-b">
           <h1>Địa chỉ của tôi</h1>
-          {data?.address ? (
-            ""
-          ) : (
-            <div>
-              <button
-                onClick={handleAddress}
-                className="flex items-center gap-2 bg-black text-white px-3 py-3 rounded-md text-sm"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="size-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M12 4.5v15m7.5-7.5h-15"
-                  />
-                </svg>
-                <span className="hidden lg:block">Thêm địa chỉ mới</span>
-              </button>
-            </div>
-          )}
+          <button
+            className="flex items-center gap-2 bg-black text-white px-3 py-3 rounded-md text-sm"
+            onClick={handleAddress}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 4.5v15m7.5-7.5h-15"
+              />
+            </svg>
+            <span className="hidden lg:block">Thêm địa chỉ mới</span>
+          </button>
         </div>
-
         <div className="px-5 py-4">
-          {data?.address ? (
-            <div className="flex justify-between items-center my-5 border-b pb-6">
+          <h2 className="py-2">Địa chỉ</h2>
+          {sortedAddresses?.map((address: Auth) => (
+            <div
+              key={address?._id}
+              className="flex justify-between items-center my-5 border-b pb-6"
+            >
               <div className="py-1">
                 <h1>
-                  {data?.address?.fullName}{" "}
+                  {address.fullName}{" "}
                   <span className="px-2 text-gray-400">|</span>{" "}
-                  <span className="text-gray-400">
-                    {data?.address?.phoneNumber}
-                  </span>
+                  <span className="text-gray-400">{address?.phoneNumber}</span>
                 </h1>
                 <div className="flex text-gray-400">
-                  <span>{data?.address?.addressDetails}</span>
+                  <span>{address.addressDetails}</span>
                   <span>-</span>
-                  <span>{data?.address?.address}</span>
+                  <span>{address.address}</span>
                 </div>
-                {/* {address.checked && (
+                {address?.checked && (
                   <div className="flex gap-3 mt-3">
                     <button className="border border-stone-300 bg-[#000000] text-white px-4 py-2 rounded-md text-sm">
                       Mặc định
                     </button>
                   </div>
-                )} */}
+                )}
               </div>
               <div>
                 <div className="hidden lg:block">
                   <div className="flex gap-2 justify-end text-blue-400 py-2">
-                    <a onClick={handleUpdateAddress} href="#">
+                    <a
+                      href="#"
+                      onClick={() => handleUpdateAddress(address?._id!)}
+                    >
                       Cập nhật
                     </a>
                     <Popconfirm
                       title="Xóa địa chỉ"
                       description="Bạn có chắc chắn muốn xóa địa chỉ này không?"
-                      onConfirm={() => deleteAddress(userId)}
+                      onConfirm={() =>
+                        handleDeleteAddress(address?._id!, address?.checked)
+                      }
                       okText="Xóa"
                       cancelText="Hủy"
                     >
                       <button>Xóa</button>
                     </Popconfirm>
                   </div>
+                  {!address?.checked && (
+                    <Button onClick={() => setDefaultAddress(address?._id)}>
+                      Thiết lập mặc định
+                    </Button>
+                  )}
                 </div>
                 <div className="block lg:hidden">
                   <svg
@@ -151,16 +172,18 @@ const Address = () => {
                 </div>
               </div>
             </div>
-          ) : (
-            "Chưa cập nhật địa chỉ"
-          )}
+          ))}
         </div>
-
-        {isOpen && <Add_Address handleAddress={handleAddress} />}
-        {isOpenUpdate && (
-          <Update_Address handleUpdateAddress={handleUpdateAddress} />
-        )}
       </div>
+      {isOpen && <Add_Address handleAddress={handleAddress} />}
+      {isOpenUpdate && (
+        <Update_Address
+          isOpenUpdate={isOpenUpdate}
+          setIsOpenUpdate={setIsOpenUpdate}
+          handleUpdateAddress={handleUpdateAddress}
+          addressId={addressId}
+        />
+      )}
     </>
   );
 };
