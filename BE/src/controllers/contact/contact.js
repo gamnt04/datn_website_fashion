ddcf12e0744c8c2c8e0d35d5e338a127d4b8731c
Trunghd09 <hoangductrung09112004@gmail.com>
2024-08-21 21:37:01 +0700

    fix/contact

--- BE/src/controllers/contact/contact.js
+++ BE/src/controllers/contact/contact.js
@@ -1,8 +1,8 @@
-import contact from "../../models/contact/contact";
+import Contact from "../../models/contact/contact";
 
 export const create_contact = async (req, res) => {
   try {
-    const data = await contact(req.body).save();
+    const data = await Contact(req.body).save();
     if (!data) {
       throw new Error(`Error creating`);
     }
@@ -16,157 +16,89 @@ export const create_contact = async (req, res) => {
       message: error.message,
     });
   }
-}
+};
 export const get_contact = async (req, res) => {
-    try {
-        const keyword = req.query.keyword;
-        let objWhere = {};
-        if (keyword) {
-        objWhere.name = new RegExp(keyword, "i");
-        }
-    
-        const data = await contact.find(objWhere);
-        if (!data) {
-        throw new Error(`Failed to get contacts`);
-        }
-        return res.status(200).json({
-        message: "Thành công",
-        data,
-        });
-    } catch (error) {
-        return res.status(400).json({
-        name: error.name,
-        message: error.message,
-        });
-    }
-    }
-    export const getById_contact = async (req, res) => {
-    try {
-        const data = await contact.findById(req.params.id);
-        if (!data) {
-        throw new Error(`Failed to get contact detail`);
-        }
-        return res.status(200).json({
-        message: "Thành công",
-        data,
-        });
-    } catch (error) {
-        return res.json({
-        name: error.name,
-        message: error.message,
-        });
-    }
-    }
-    export const delete_contact = async (req, res) => {
-    try {
-        const data = await contact.findByIdAndDelete(req.params.id);
-        if (!data) {
-        throw new Error(`Failed to delete contact`);
-        }
-        return res.status(200).json({
-        message: "Xóa thành công",
-        data,
-        });
-    } catch (error) {
-        return res.json({
-        name: error.name,
-        message: error.message,
-        });
-    }
+  try {
+    const keyword = req.query.keyword;
+    let objWhere = {};
+    if (keyword) {
+      objWhere.name = new RegExp(keyword, "i");
     }
 
+    const data = await Contact.find(objWhere);
+    if (!data) {
+      throw new Error(`Failed to get contacts`);
+    }
+    return res.status(200).json({
+      message: "Thành công",
+      data,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      name: error.name,
+      message: error.message,
+    });
+  }
+};
+export const getById_contact = async (req, res) => {
+  try {
+    const data = await Contact.findById(req.params.id);
+    if (!data) {
+      throw new Error(`Failed to get contact detail`);
+    }
+    return res.status(200).json({
+      message: "Thành công",
+      data,
+    });
+  } catch (error) {
+    return res.json({
+      name: error.name,
+      message: error.message,
+    });
+  }
+};
+export const delete_contact = async (req, res) => {
+  try {
+    const data = await Contact.findByIdAndDelete(req.params.id);
+    if (!data) {
+      throw new Error(`Failed to delete contact`);
+    }
+    return res.status(200).json({
+      message: "Xóa thành công",
+      data,
+    });
+  } catch (error) {
+    return res.json({
+      name: error.name,
+      message: error.message,
+    });
+  }
+};
 
+export const update = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { response_content, responder_email } = req.body;
 
+    const contact = await Contact.findById(id);
 
+    if (!contact) {
+      return res.status(404).json({ message: "Liên hệ không tồn tại" });
+    }
 
+    contact.response_content = response_content;
+    contact.responder_email = responder_email;
+    contact.response_date = new Date();
 
+    await contact.save();
 
+    res.status(200).json({ message: "Phản hồi đã được cập nhật", contact });
+  } catch (error) {
+    res.status(500).json({ message: "Lỗi cập nhật phản hồi", error });
+  }
+};
 
-
-
-
-
-
-
-    // import contact from "../../models/contact/contact";
-
-    // export const create_contact = async (req, res) => {
-    //   try {
-    //     const data = await contact(req.body).save();
-    //     if (!data) {
-    //       throw new Error(`Error creating`);
-    //     }
-    //     return res.status(200).json({
-    //       message: "Bạn đã tạo thành công",
-    //       data,
-    //     });
-    //   } catch (error) {
-    //     return res.json({
-    //       name: error.name,
-    //       message: error.message,
-    //     });
-    //   }
-    // }
-    // export const get_contact = async (req, res) => {
-    //     try {
-    //         const keyword = req.query.keyword;
-    //         let objWhere = {};
-    //         if (keyword) {
-    //         objWhere.name = new RegExp(keyword, "i");
-    //         }
-        
-    //         const data = await contact.find(objWhere);
-    //         if (!data) {
-    //         throw new Error(`Failed to get contacts`);
-    //         }
-    //         return res.status(200).json({
-    //         message: "Thành công",
-    //         data,
-    //         });
-    //     } catch (error) {
-    //         return res.status(400).json({
-    //         name: error.name,
-    //         message: error.message,
-    //         });
-    //     }
-    //     }
-    //     export const getById_contact = async (req, res) => {
-    //     try {
-    //         const data = await contact.findById(req.params.id);
-    //         if (!data) {
-    //         throw new Error(`Failed to get contact detail`);
-    //         }
-    //         return res.status(200).json({
-    //         message: "Thành công",
-    //         data,
-    //         });
-    //     } catch (error) {
-    //         return res.json({
-    //         name: error.name,
-    //         message: error.message,
-    //         });
-    //     }
-    //     }
-    //     export const delete_contact = async (req, res) => {
-    //     try {
-    //         const data = await contact.findByIdAndDelete(req.params.id);
-    //         if (!data) {
-    //         throw new Error(`Failed to delete contact`);
-    //         }
-    //         return res.status(200).json({
-    //         message: "Xóa thành công",
-    //         data,
-    //         });
-    //     } catch (error) {
-    //         return res.json({
-    //         name: error.name,
-    //         message: error.message,
-    //         });
-    //     }
-    //     }
-        
-        
-//     import contact from "../../models/contact/contact";
+// import contact from "../../models/contact/contact";
 
 // export const create_contact = async (req, res) => {
 //   try {
@@ -192,7 +124,7 @@ export const get_contact = async (req, res) => {
 //         if (keyword) {
 //         objWhere.name = new RegExp(keyword, "i");
 //         }
-    
+
 //         const data = await contact.find(objWhere);
 //         if (!data) {
 //         throw new Error(`Failed to get contacts`);
@@ -242,5 +174,80 @@ export const get_contact = async (req, res) => {
 //         });
 //     }
 //     }
-    
-    
+
+//     import contact from "../../models/contact/contact";
+
+// export const create_contact = async (req, res) => {
+//   try {
+//     const data = await contact(req.body).save();
+//     if (!data) {
+//       throw new Error(`Error creating`);
+//     }
+//     return res.status(200).json({
+//       message: "Bạn đã tạo thành công",
+//       data,
+//     });
+//   } catch (error) {
+//     return res.json({
+//       name: error.name,
+//       message: error.message,
+//     });
+//   }
+// }
+// export const get_contact = async (req, res) => {
+//     try {
+//         const keyword = req.query.keyword;
+//         let objWhere = {};
+//         if (keyword) {
+//         objWhere.name = new RegExp(keyword, "i");
+//         }
+
+//         const data = await contact.find(objWhere);
+//         if (!data) {
+//         throw new Error(`Failed to get contacts`);
+//         }
+//         return res.status(200).json({
+//         message: "Thành công",
+//         data,
+//         });
+//     } catch (error) {
+//         return res.status(400).json({
+//         name: error.name,
+//         message: error.message,
+//         });
+//     }
+//     }
+//     export const getById_contact = async (req, res) => {
+//     try {
+//         const data = await contact.findById(req.params.id);
+//         if (!data) {
+//         throw new Error(`Failed to get contact detail`);
+//         }
+//         return res.status(200).json({
+//         message: "Thành công",
+//         data,
+//         });
+//     } catch (error) {
+//         return res.json({
+//         name: error.name,
+//         message: error.message,
+//         });
+//     }
+//     }
+//     export const delete_contact = async (req, res) => {
+//     try {
+//         const data = await contact.findByIdAndDelete(req.params.id);
+//         if (!data) {
+//         throw new Error(`Failed to delete contact`);
+//         }
+//         return res.status(200).json({
+//         message: "Xóa thành công",
+//         data,
+//         });
+//     } catch (error) {
+//         return res.json({
+//         name: error.name,
+//         message: error.message,
+//         });
+//     }
+//     }
