07d6a4735bbd016539997fb3dcc55c0bca09fb81
ducna34806 <ducnaph34806@fpt.edu.vn>
2024-09-26 19:15:12 +0700

    be use voucher

--- BE/src/controllers/Voucher/voucher.js
+++ BE/src/controllers/Voucher/voucher.js
@@ -67,3 +67,67 @@ export const updateVoucher = async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 };
+
+export const useVoucher = async (req, res) => {
+  const { code_voucher, totalAmount } = req.body;
+
+  try {
+    const voucher = await Voucher.findOne({ code_voucher, isActive: true });
+    if (!voucher) {
+      return res
+        .status(404)
+        .json({ message: "Không tìm thấy mã giảm giá hoặc đã hết hạn" });
+    }
+    // Kiểm tra nếu voucher chỉ dành cho một số người dùng
+    if (
+      voucher.allowedUsers.length > 0 &&
+      !voucher.allowedUsers.includes(userId)
+    ) {
+      return res
+        .status(403)
+        .json({ message: "You are not authorized to use this voucher" });
+    }
+
+    // Kiểm tra nếu voucher đã hết số lượng
+    if (voucher.usedCount >= voucher.quantity_voucher) {
+      return res
+        .status(400)
+        .json({ message: "Mã giảm giá đã đạt hạn mức sử dụng" });
+    }
+
+    // Kiểm tra ngày bắt đầu
+    if (new Date(voucher.startDate) > new Date()) {
+      return res.status(400).json({ message: "Mã giảm giá chưa được áp dụng" });
+    }
+    // Kiểm tra ngày hết hạn
+    if (new Date(voucher.expirationDate) < new Date()) {
+      return res.status(400).json({ message: "Mã giảm giá đã hết hạn " });
+    }
+
+    // Kiểm tra điều kiện số tiền tối thiểu
+    if (totalAmount < voucher.minimumSpend) {
+      return res.status(400).json({
+        message: `Bạn cần chi tiêu tối thiểu ${voucher.minimumSpend} để có thể sử dụng mã giảm giá này`,
+      });
+    }
+
+    // Tính giá trị giảm giá
+    let discount = 0;
+    if (voucher.discountType === "percentage") {
+      discount = (voucher.discountValue / 100) * totalAmount;
+    } else {
+      discount = voucher.discountValue;
+    }
+
+    voucher.usedCount += 1;
+    await voucher.save();
+
+    res.json({
+      message: "Áp dụng mã giảm giá thành công!",
+      discount,
+      finalAmount: totalAmount - discount,
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
