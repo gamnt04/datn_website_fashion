ed718f369e88bb68ca9dda5f5e961c690fc3f915
ducna34806 <ducnaph34806@fpt.edu.vn>
2024-09-26 19:05:19 +0700

    be delete voucher

--- BE/src/routers/voucher.js
+++ BE/src/routers/voucher.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {
   addVoucher,
+  deleteVoucher,
   getVoucher,
   getVoucherById,
 } from "../controllers/Voucher/voucher";
@@ -9,6 +10,7 @@ const Routes_voucher = express.Router();
 
 Routes_voucher.get("/voucher", getVoucher);
 Routes_voucher.get("/voucher/:id", getVoucherById);
+Routes_voucher.delete("/voucher/:id", deleteVoucher);
 
 Routes_voucher.post("/voucher", addVoucher);
 
