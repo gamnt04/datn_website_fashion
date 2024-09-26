af13588915733680be9d6257c59b24f71e7f9741
ducna34806 <ducnaph34806@fpt.edu.vn>
2024-09-26 19:01:12 +0700

    fix be list voucher

--- BE/src/routers/voucher.js
+++ BE/src/routers/voucher.js
@@ -1,9 +1,15 @@
 import express from "express";
-import { addVoucher, getVoucher } from "../controllers/Voucher/voucher";
+import {
+  addVoucher,
+  getVoucher,
+  getVoucherById,
+} from "../controllers/Voucher/voucher";
 
 const Routes_voucher = express.Router();
 
 Routes_voucher.get("/voucher", getVoucher);
+Routes_voucher.get("/voucher/:id", getVoucherById);
+
 Routes_voucher.post("/voucher", addVoucher);
 
 export default Routes_voucher;
