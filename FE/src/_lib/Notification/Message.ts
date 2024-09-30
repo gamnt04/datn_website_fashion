3419f3c86cd7c798cbe8c940c1ab9f0f0c39da9c
ducna34806 <ducnaph34806@fpt.edu.vn>
2024-09-30 19:42:41 +0700

    fix conflic

--- FE/src/_lib/Notification/Message.ts
+++ FE/src/_lib/Notification/Message.ts
@@ -15,7 +15,6 @@ export const add_Notification = async (dataMessage: {
 };
 
 export const get_Notification_By_User = async (userId: string | number) => {
-<<<<<<< HEAD
   try {
     const { data } = await instance.get(`/notification/${userId}`);
     return data;
@@ -23,30 +22,21 @@ export const get_Notification_By_User = async (userId: string | number) => {
     console.log(error);
   }
 };
-export const getAll_Notification = async () => {
-  try {
-    const { data } = await instance.get(`/notification`);
-    console.log(data);
+// export const getAll_Notification = async () => {
+//   try {
+//     const { data } = await instance.get(`/notification`);
+//     console.log(data);
 
+//     return data;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+export const getAll_Notification = async (role: string) => {
+  try {
+    const { data } = await instance.get(`/notification`, { params: { role } });
     return data;
   } catch (error) {
     console.log(error);
   }
 };
-=======
-    try {
-        const { data } = await instance.get(`/notification/${userId}`);
-        return data;
-    } catch (error) {
-        console.log(error);
-    }
-}
-export const getAll_Notification = async (role: string) => {
-    try {
-        const { data } = await instance.get(`/notification`, { params: { role } });
-        return data;
-    } catch (error) {
-        console.log(error);
-    }
-}
->>>>>>> 383b071ecc5dafac8a5df1eccd334852f438a9b5
