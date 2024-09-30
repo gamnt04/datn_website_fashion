44d8aaef243ebbabd7bca846b8ceb5b6db089f34
ducna34806 <ducnaph34806@fpt.edu.vn>
2024-09-30 20:16:35 +0700

    fe list voucher

---
+++ FE/src/common/interfaces/Voucher.tsx
@@ -0,0 +1,15 @@
+export interface IVoucher {
+  _id: string;
+  name_voucher: string;
+  code_voucher: string;
+  description_voucher: string;
+  quantity_voucher: number;
+  usedCount: number;
+  discountType: string;
+  discountValue: number;
+  minimumSpend: number;
+  allowedUsers: string[];
+  startDate: Date;
+  expirationDate: Date;
+  isActive: boolean;
+}
