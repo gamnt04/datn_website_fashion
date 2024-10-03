e944c65ebe23850681ce549cd224dfbc6a791bbd
Trunghd09 <hoangductrung09112004@gmail.com>
2024-10-03 23:38:55 +0700

    sửa giao diện add vs edit Voucher

--- FE/src/pages/Admin/Voucher/AddVoucher.tsx
+++ FE/src/pages/Admin/Voucher/AddVoucher.tsx
@@ -13,7 +13,7 @@ import instance from "../../../configs/axios";
 import TextArea from "antd/es/input/TextArea";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-
+import { FaRandom } from "react-icons/fa";
 type FieldType = {
   name_voucher: string;
   code_voucher: string;
@@ -97,145 +97,203 @@ const AddVoucher = () => {
   if (isLoading) return <div>Loading...</div>;
 
   return (
-    <div className="mt-20 ml-10">
-      {contextHolder}
-      <Form
-        name="basic"
-        labelCol={{ span: 8 }}
-        wrapperCol={{ span: 16 }}
-        style={{ maxWidth: 600 }}
-        layout="vertical"
-        initialValues={{ remember: true }}
-        onFinish={onFinish}
-        autoComplete="off"
-        form={form}
-      >
-        <Form.Item<FieldType>
-          label="Tên mã giảm giá"
-          name="name_voucher"
-          rules={[
-            { required: true, message: "Vui lòng nhập tên mã giảm giá!" },
-          ]}
-        >
-          <Input />
-        </Form.Item>
+    <div className="mt-20">
+      <div className="border-b border-gray-900/10 pb-12">
+        <h2 className="text-2xl font-semibold leading-7 text-gray-900 ml-16 ">
+          Thêm Voucher
+        </h2>
+        <div className=" ml-10 p-6 ">
+          {contextHolder}
+          <Form
+            name="basic"
+            labelCol={{ span: 8 }}
+            wrapperCol={{ span: 16 }}
+            layout="vertical"
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            autoComplete="off"
+            form={form}
+            className="max-w-full"
+          >
+            {/* Use flexbox to split form into 2 columns */}
+            <div className="flex flex-wrap -mx-4">
+              {/* Column 1 */}
+              <div className="w-full md:w-1/2 px-4">
+                <Form.Item<FieldType>
+                  label="Tên mã giảm giá"
+                  name="name_voucher"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Vui lòng nhập tên mã giảm giá!",
+                    },
+                  ]}
+                >
+                  <Input className="h-10" />
+                </Form.Item>
+                <Form.Item
+                  label="Loại mã giảm giá"
+                  name="discountType"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Vui lòng chọn loại mã giảm giá!",
+                    },
+                  ]}
+                >
+                  <Select className="h-10">
+                    <Select.Option value="percentage">
+                      Percentage (%)
+                    </Select.Option>
+                    <Select.Option value="fixed">Fixed (VND)</Select.Option>
+                  </Select>
+                </Form.Item>
+                <Form.Item<FieldType>
+                  label="Điều kiện mã giảm giá"
+                  name="minimumSpend"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Vui lòng nhập điều kiện mã giảm giá!",
+                    },
+                  ]}
+                >
+                  <InputNumber className="w-full h-10" />
+                </Form.Item>
 
-        <Form.Item
-          label="Mã giảm giá"
-          name="code_voucher"
-          rules={[{ required: true, message: "Vui lòng nhập mã giảm giá!" }]}
-        >
-          <Input
-            addonAfter={
-              <Button type="default" onClick={generateRandomCode}>
-                Random
+                <Form.Item<FieldType>
+                  label="Ngày bắt đầu mã giảm giá"
+                  name="startDate"
+                  rules={[
+                    { required: true, message: "Vui lòng nhập ngày bắt đầu!" },
+                  ]}
+                >
+                  <DatePicker showTime className="w-full h-10" />
+                </Form.Item>
+
+                <Form.Item<FieldType>
+                  label="Mô tả mã giảm giá"
+                  name="description_voucher"
+                  rules={[{ required: true, message: "Vui lòng nhập mô tả!" }]}
+                >
+                  <TextArea rows={4} />
+                </Form.Item>
+              </div>
+
+              {/* Column 2 */}
+              <div className="w-full md:w-1/2 px-4">
+                <Form.Item
+                  label="Mã giảm giá"
+                  name="code_voucher"
+                  rules={[
+                    { required: true, message: "Vui lòng nhập mã giảm giá!" },
+                  ]}
+                >
+                  <Input
+                    addonAfter={
+                      <Button
+                        onClick={generateRandomCode}
+                        className="flex items-center justify-center h-full w-12"
+                        style={{
+                          border: "none",
+                          backgroundColor: "transparent",
+                        }}
+                      >
+                        <FaRandom
+                          style={{
+                            transform: "rotate(180deg)",
+                            fontSize: "16px",
+                          }}
+                        />{" "}
+                      </Button>
+                    }
+                    className="h-10"
+                  />
+                </Form.Item>
+                <Form.Item
+                  label="Giá trị mã giảm giá"
+                  name="discountValue"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Vui lòng nhập giá trị mã giảm giá!",
+                    },
+                  ]}
+                >
+                  <InputNumber className="w-full h-10" />
+                </Form.Item>
+                <Form.Item<FieldType>
+                  label="Số lượng mã giảm giá"
+                  name="quantity_voucher"
+                  rules={[
+                    { required: true, message: "Vui lòng nhập số lượng!" },
+                  ]}
+                >
+                  <InputNumber className="w-full h-10" />
+                </Form.Item>
+                <Form.Item<FieldType>
+                  label="Ngày kết thúc mã giảm giá"
+                  name="expirationDate"
+                  rules={[
+                    { required: true, message: "Vui lòng nhập ngày kết thúc!" },
+                  ]}
+                >
+                  <DatePicker showTime className="w-full h-10" />
+                </Form.Item>
+
+                <Form.Item<FieldType>
+                  label="Người sử dụng mã giảm giá"
+                  name="allowedUsers"
+                >
+                  <div className="flex items-center">
+                    <Select
+                      mode="multiple"
+                      style={{
+                        width: "90%",
+                        minHeight: "40px", // Set a fixed height to maintain the arrow icon's position
+                      }}
+                      placeholder="Người dùng"
+                      className="mt-2"
+                      options={[
+                        { value: "all", label: "Chọn tất cả người dùng" },
+                        ...data?.data.map((user: any) => ({
+                          value: user._id,
+                          label: user.userName,
+                        })),
+                      ]}
+                      onChange={handleSelectChange}
+                      value={selectedUsers}
+                      dropdownStyle={{ maxHeight: 250, overflowY: "auto" }} // Limit dropdown height
+                      maxTagCount={4} // Shows up to 5 selected users
+                      maxTagPlaceholder={(omittedValues) =>
+                        `+${omittedValues.length} người khác`
+                      }
+                      allowClear
+                    />
+                    <Button
+                      onClick={handleDeselectAll}
+                      className=" absolute left-[390px] top-2 ml-2 h-8 border-red-400 text-red-400"
+                    >
+                      X
+                    </Button>
+                  </div>
+                </Form.Item>
+              </div>
+            </div>
+
+            {/* Submit Button */}
+            <Form.Item className="h-20">
+              <Button
+                type="primary"
+                htmlType="submit"
+                className="w-1/2 h-10 text-xl"
+              >
+                Submit
               </Button>
-            }
-          />
-        </Form.Item>
-
-        <Form.Item<FieldType>
-          label="Mô tả mã giảm giá"
-          name="description_voucher"
-          rules={[{ required: true, message: "Vui lòng nhập mô tả!" }]}
-        >
-          <TextArea rows={4} />
-        </Form.Item>
-
-        <Form.Item<FieldType>
-          label="Số lượng mã giảm giá"
-          name="quantity_voucher"
-          rules={[{ required: true, message: "Vui lòng nhập số lượng!" }]}
-        >
-          <InputNumber />
-        </Form.Item>
-
-        <Form.Item
-          label="Loại mã giảm giá"
-          name="discountType"
-          rules={[
-            { required: true, message: "Vui lòng chọn loại mã giảm giá!" },
-          ]}
-        >
-          <Select>
-            <Select.Option value="percentage">Percentage (%)</Select.Option>
-            <Select.Option value="fixed">Fixed (VND)</Select.Option>
-          </Select>
-        </Form.Item>
-
-        <Form.Item
-          label="Giá trị mã giảm giá"
-          name="discountValue"
-          rules={[
-            { required: true, message: "Vui lòng nhập giá trị mã giảm giá!" },
-          ]}
-        >
-          <InputNumber />
-        </Form.Item>
-
-        <Form.Item<FieldType>
-          label="Điều kiện mã giảm giá"
-          name="minimumSpend"
-          rules={[
-            { required: true, message: "Vui lòng nhập điều kiện mã giảm giá!" },
-          ]}
-        >
-          <InputNumber />
-        </Form.Item>
-
-        <Form.Item<FieldType>
-          label="Người sử dụng mã giảm giá"
-          name="allowedUsers"
-        >
-          <div className="flex items-center space-x-2">
-            <Select
-              mode="multiple"
-              style={{ width: "100%" }}
-              placeholder="Người dùng"
-              className=" mt-2"
-              options={[
-                { value: "all", label: "Chọn tất cả người dùng" }, // "Chọn tất cả người dùng"
-                ...data?.data.map((user: any) => ({
-                  value: user._id,
-                  label: user.userName,
-                })),
-              ]}
-              onChange={handleSelectChange}
-              value={selectedUsers} // Sử dụng trạng thái để quản lý danh sách người dùng đã chọn
-            />
-            {/* Nút bỏ chọn tất cả */}
-            <Button
-              onClick={handleDeselectAll}
-              style={{ marginTop: "10px" }}
-              className="absolute left-[400px] top-0 h-7 border-red-400 text-red-400"
-            >
-              X
-            </Button>
-          </div>
-        </Form.Item>
-
-        <Form.Item<FieldType>
-          label="Ngày bắt đầu mã giảm giá"
-          name="startDate"
-          rules={[{ required: true, message: "Vui lòng nhập ngày bắt đầu!" }]}
-        >
-          <DatePicker showTime />
-        </Form.Item>
-
-        <Form.Item<FieldType>
-          label="Ngày kết thúc mã giảm giá"
-          name="expirationDate"
-          rules={[{ required: true, message: "Vui lòng nhập ngày kết thúc!" }]}
-        >
-          <DatePicker showTime />
-        </Form.Item>
-
-        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-        </Form.Item>
-      </Form>
+            </Form.Item>
+          </Form>
+        </div>
+      </div>
     </div>
   );
 };
