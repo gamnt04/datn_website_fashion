94a74c77af5adddf6c91a90d7b5930a55f33dff1
BaoChung-Cloud <chungkbph38477@fpt.edu.vn>
2024-10-08 22:38:43 +0700

    new

--- BE/src/routers/message.js
+++ BE/src/routers/message.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import {
-  getMessagesByUserId,
+  getMessagesBetweenUsers,
   sendMessage
 } from "../controllers/Message/message";
 
 const Router_Message = Router();
 Router_Message.post("/message/send", sendMessage);
-Router_Message.get("/message/:userId", getMessagesByUserId);
+Router_Message.get("/messages/:userId1/:userId2", getMessagesBetweenUsers);
 
 export default Router_Message;
