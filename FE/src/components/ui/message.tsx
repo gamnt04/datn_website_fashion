4f59d171719d5d082af8a5c958f644f14b333d4d
BaoChung-Cloud <chungkbph38477@fpt.edu.vn>
2024-10-11 20:35:22 +0700

    new

--- FE/src/components/ui/message.tsx
+++ FE/src/components/ui/message.tsx
@@ -1,3 +1,4 @@
+// Component Message.jsx
 import { useState } from "react";
 import message from "../../assets/Images/Logo/logoMessage.jpg";
 import Chat from "../../pages/Client/Chat/Chat";
@@ -6,28 +7,41 @@ const Message = () => {
   const [isChatOpen, setIsChatOpen] = useState(false);
 
   const toggleChat = (event) => {
-    event.stopPropagation(); // Ngăn không để sự kiện lan truyền lên trên, gây đóng popup
+    if (event) {
+      event.stopPropagation();
+    }
     setIsChatOpen(!isChatOpen);
   };
 
   return (
-    <div className="fixed right-4 bottom-24 z-50 cursor-pointer">
-      <div className="relative w-[55px] h-[55px]" onClick={toggleChat}>
+    <div className="fixed right-4 bottom-24 z-50">
+      {/* Biểu tượng tin nhắn */}
+      <div className="w-[55px] h-[55px] cursor-pointer" onClick={toggleChat}>
         <img
           src={message}
           alt="Message Icon"
           className="rounded-full w-full h-full object-cover"
-          // Xóa onClick ở đây vì sự kiện đã được bao trùm bởi div cha
         />
-        {isChatOpen && (
-          <div
-            className="fixed inset-0 flex justify-end pr-[90px] mt-[230px]"
-            onClick={(event) => event.stopPropagation()} // Chặn sự kiện nhấp để không đóng chat
-          >
-            <Chat />
-          </div>
-        )}
       </div>
+
+      {/* Popup chat */}
+      {isChatOpen && (
+        <div
+          className="fixed inset-0 z-40 flex justify-end items-end"
+          onClick={toggleChat}
+        >
+          {/* Lớp phủ mờ */}
+          <div className="absolute inset-0 bg-black bg-opacity-50"></div>
+
+          {/* Nội dung chat */}
+          <div
+            className="relative w-[350px] h-[500px] mr-4 mb-24"
+            onClick={(event) => event.stopPropagation()}
+          >
+            <Chat onClose={toggleChat} />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
