da692de09248aa11f50fd3162b3e6dab21d8ab9b
Trunghd09 <hoangductrung09112004@gmail.com>
2024-12-07 20:30:15 +0700

    sửa lại loading

--- FE/src/pages/Admin/Category/Create/Create.tsx
+++ FE/src/pages/Admin/Category/Create/Create.tsx
@@ -7,6 +7,7 @@ import { Input } from "../../../../components/ui/Input";
 import { UploadImage } from "../../../../systems/utils/uploadImage";
 import { message, Spin } from "antd";
 import { LoadingOutlined } from "@ant-design/icons";
+import Loading from "../../../../components/base/Loading/Loading";
 const CreateComponent = () => {
   const [showMessage, setShowMessage] = useState(false);
   const [imagePreview, setImagePreview] = useState<string | null>(null);
@@ -69,7 +70,7 @@ const CreateComponent = () => {
 
   if (isPending) {
     return (
-      <div className="flex justify-center items-center h-screen">
+      <div className="flex justify-center items-center h-96">
         <Spin indicator={<LoadingOutlined spin />} size="large" />
       </div>
     );
@@ -77,84 +78,88 @@ const CreateComponent = () => {
 
   return (
     <div>
-      <form onSubmit={handleSubmit(handleSubmitForm)}>
-        <div className="space-y-12">
-          <div className="border-b border-gray-900/10 pb-12">
-            <h2 className="text-2xl font-semibold leading-7 text-gray-900 text-center">
-              Thêm danh mục
-            </h2>
-            <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6 justify-center">
-              <div className="sm:col-span-6 flex justify-center">
-                <div className="sm:col-span-2">
-                  <label
-                    htmlFor="name"
-                    className="block text-[16px] font-medium leading-6 text-gray-900"
-                  >
-                    Tên danh mục
-                  </label>
-                  <div className="mt-2">
-                    <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-lg">
-                      <Input
-                        type="text"
-                        placeholder="Nhập tên danh mục..."
-                        {...register("name_category", { required: true })}
-                      />
-                    </div>
-                    {errors.name_category && (
-                      <p className="text-red-600">
-                        Tên danh mục bắt buộc nhập!
-                      </p>
-                    )}
-                  </div>
-                  <label
-                    htmlFor="image_category"
-                    className="block text-[16px] font-medium leading-6 text-gray-900 mt-4"
-                  >
-                    Hình ảnh danh mục
-                  </label>
-                  <div className="mt-2 flex items-center">
-                    <div className="relative flex items-center">
-                      <div
-                        className={`flex items-center justify-center w-32 h-32 border border-gray-300 rounded-lg bg-gray-100 mr-2`}
-                      >
-                        <span className="text-3xl text-gray-500">+</span>
-                      </div>
-                      {imagePreview && (
-                        <img
-                          src={imagePreview}
-                          alt="Image Preview"
-                          className="w-32 h-32 object-cover border border-gray-300 rounded-lg"
+      {isPending ? (
+        <Loading />
+      ) : (
+        <form onSubmit={handleSubmit(handleSubmitForm)}>
+          <div className="space-y-12">
+            <div className="border-b border-gray-900/10 pb-12">
+              <h2 className="text-2xl font-semibold leading-7 text-gray-900 text-center">
+                Thêm danh mục
+              </h2>
+              <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6 justify-center">
+                <div className="sm:col-span-6 flex justify-center">
+                  <div className="sm:col-span-2">
+                    <label
+                      htmlFor="name"
+                      className="block text-[16px] font-medium leading-6 text-gray-900"
+                    >
+                      Tên danh mục
+                    </label>
+                    <div className="mt-2">
+                      <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-lg">
+                        <Input
+                          type="text"
+                          placeholder="Nhập tên danh mục..."
+                          {...register("name_category", { required: true })}
                         />
+                      </div>
+                      {errors.name_category && (
+                        <p className="text-red-600">
+                          Tên danh mục bắt buộc nhập!
+                        </p>
                       )}
-                      <Input
-                        type="file"
-                        {...register("image_category", { required: true })}
-                        className="absolute inset-0 opacity-0 cursor-pointer"
-                      />
+                    </div>
+                    <label
+                      htmlFor="image_category"
+                      className="block text-[16px] font-medium leading-6 text-gray-900 mt-4"
+                    >
+                      Hình ảnh danh mục
+                    </label>
+                    <div className="mt-2 flex items-center">
+                      <div className="relative flex items-center">
+                        <div
+                          className={`flex items-center justify-center w-32 h-32 border border-gray-300 rounded-lg bg-gray-100 mr-2`}
+                        >
+                          <span className="text-3xl text-gray-500">+</span>
+                        </div>
+                        {imagePreview && (
+                          <img
+                            src={imagePreview}
+                            alt="Image Preview"
+                            className="w-32 h-32 object-cover border border-gray-300 rounded-lg"
+                          />
+                        )}
+                        <Input
+                          type="file"
+                          {...register("image_category", { required: true })}
+                          className="absolute inset-0 opacity-0 cursor-pointer"
+                        />
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className="mt-6 flex flex-col items-center gap-y-4">
-          {showMessage && (
-            <Message
-              message="Thêm danh mục thành công!"
-              type="success"
-              timeout={2000} // Tự động tắt sau 2 giây
-              openMessage={showMessage}
-            />
-          )}
-          <button
-            className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            type="submit"
-          >
-            {isPending ? "Đang thêm" : "Xác nhận"}
-          </button>
-        </div>
-      </form>
+          <div className="mt-6 flex flex-col items-center gap-y-4">
+            {showMessage && (
+              <Message
+                message="Thêm danh mục thành công!"
+                type="success"
+                timeout={2000} // Tự động tắt sau 2 giây
+                openMessage={showMessage}
+              />
+            )}
+            <button
+              className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              type="submit"
+            >
+              {isPending ? "Đang thêm" : "Xác nhận"}
+            </button>
+          </div>
+        </form>
+      )}
     </div>
   );
 };
