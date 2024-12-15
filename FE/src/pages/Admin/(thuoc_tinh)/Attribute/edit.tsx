04f81e957a01698e49875333faf0e576f04f63cb
tulam120604 <saudoi1204@gmail.com>
2024-12-16 02:04:31 +0700

    update

--- FE/src/pages/Admin/(thuoc_tinh)/Attribute/edit.tsx
+++ FE/src/pages/Admin/(thuoc_tinh)/Attribute/edit.tsx
@@ -5,12 +5,15 @@ import { Dispatch_thuoc_tinh, Lay_thuoc_tinh } from "../../../../API/Dispatch/sl
 import useLocalStorage from "../../../../common/hooks/Storage/useStorage";
 import { Button, Form, FormProps, Input, Spin, Upload } from "antd";
 import { SketchPicker } from "react-color";
-import { LoadingOutlined, PlusOutlined } from "@ant-design/icons";
+import { LoadingOutlined } from "@ant-design/icons";
+import { FilePenLine } from "lucide-react";
+import { UploadImage } from "../../../../systems/utils/uploadImage";
 
 
 export default function Edit_thuoc_tinh({ props }: any) {
     const location_url = useLocation();
     const urlParams = new URLSearchParams(location_url.search);
+    const [loadingUpload, setLoadingUpload] = useState<boolean>(false)
     const editId = urlParams.get('_id');
     const [symbol, setSymbol] = useState<string>('');
     const [user] = useLocalStorage("user", {});
@@ -19,6 +22,7 @@ export default function Edit_thuoc_tinh({ props }: any) {
         id_thuoc_tinh: editId
     });
     const [imageFile, setImageFile] = useState<File[]>([]);
+    const [status_change_img, setStatus_change_img] = useState<boolean>(false);
     const { mutate, isLoading: loading, isError: error } = Dispatch_thuoc_tinh('EDIT')
     const [form] = Form.useForm();
 
@@ -27,8 +31,19 @@ export default function Edit_thuoc_tinh({ props }: any) {
             form.setFieldsValue({
                 ten_thuoc_tinh: data?.ten_thuoc_tinh
             });
+            const fileList = [{
+                uid: '-1',
+                name: 'image.jpg',
+                status: 'done',
+                url: data?.symbol_thuoc_tinh,
+            }];
+            form.setFieldsValue({
+                symbol_thuoc_tinh: fileList,
+            });
+            setImageFile(data?.symbol_thuoc_tinh)
         }
     }, [data, form]);
+    console.log(imageFile)
     const handleCancel = () => {
         const url_location: any = window.location;
         const url = new URL(url_location);
@@ -41,14 +56,25 @@ export default function Edit_thuoc_tinh({ props }: any) {
         const files =
             imageItem?.fileList?.map((file: any) => file.originFileObj) || [];
         setImageFile(files);
+        setStatus_change_img(true);
     };
 
-    const onFinish: FormProps<any>['onFinish'] = (values) => {
+    const onFinish: FormProps<any>['onFinish'] = async (values) => {
+        let imageUrl = '';
+        if (status_change_img) {
+            if (data?.the_loai_thuoc_tinh === 'ux_image') {
+                setLoadingUpload(true);
+                imageUrl = await UploadImage(imageFile[0]);
+                setLoadingUpload(false)
+            }
+        }
+        console.log(symbol)
         const data_request = {
             _id: editId,
             ...data,
             ten_thuoc_tinh: values?.ten_thuoc_tinh,
-            symbol_thuoc_tinh: data?.the_loai_thuoc_tinh === 'ux_image' ? imageFile : data?.the_loai_thuoc_tinh === 'ux_color' ? symbol : ''
+            symbol_thuoc_tinh: data?.the_loai_thuoc_tinh === 'ux_image' ? (status_change_img ? imageUrl
+                : data?.symbol_thuoc_tinh) : data?.the_loai_thuoc_tinh === 'ux_color' ? (symbol ? symbol : data?.symbol_thuoc_tinh) : ''
         }
         mutate(data_request);
         props?.setIsModalOpen(false);
@@ -63,23 +89,23 @@ export default function Edit_thuoc_tinh({ props }: any) {
     }
     {
         (isLoading) &&
-        <div className="flex justify-center items-center h-screen">
-            <Spin indicator={<LoadingOutlined spin />} size="large" />
-        </div>
+            <div className="flex justify-center items-center h-screen">
+                <Spin indicator={<LoadingOutlined spin />} size="large" />
+            </div>
     }
     if (isError || error) return <span>Error...</span>;
     return (
         <div className={`${props?.isModalOpen ? 'translate-y-0' : 'translate-y-[-200%]'} fixed top-0 w-screen left-0 h-screen z-[100]`}>
             <div className={`bg-[#33333333] w-screen h-screen fixed top-0 left-0 z-[100]`} onClick={handleCancel}></div>
-            <div className={`${props?.isModalOpen ? '-translate-y-1/2' : '-translate-y-[200%]'} fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-auto py-5 px-14 rounded bg-white z-[110]`}>
-                {(loading) &&
-                    <div className="flex justify-center items-center h-screen">
-                        <Spin indicator={<LoadingOutlined spin />} size="large" />
-                    </div>
-                }
+            {(loading || loadingUpload) &&
+                <div className="fixed bg-[#33333333] z-[200] top-0 left-0 w-screen flex justify-center items-center h-screen">
+                    <Spin indicator={<LoadingOutlined spin />} size="large" />
+                </div>
+            }
+            <div className={`${props?.isModalOpen ? '-translate-y-1/2' : '-translate-y-[200%]'} fixed duration-200 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-auto py-5 px-14 rounded bg-white z-[110]`}>
                 <span className="text-lg font-semibold">Cập nhật thuộc tính {data?.ten_thuoc_tinh}</span>
                 <Form className="mt-10"
-                    form={form}  // Liên kết form instance với Form
+                    form={form}
                     name="basic"
                     onFinish={onFinish}
                     onFinishFailed={onFinishFailed}
@@ -108,19 +134,25 @@ export default function Edit_thuoc_tinh({ props }: any) {
                     }
                     {
                         data?.the_loai_thuoc_tinh === 'ux_image' &&
-                        <Upload
-                            listType="picture-card"
-                            beforeUpload={() => false}
-                            onChange={handleImageChange}
-                            className="mt-2"
-                            maxCount={1}
-                            // defaultFileList={data?.the_loai_thuoc_tinh}
-                            name="the_loai_thuoc_tinh"
-                        >
-                            <button type="button">
-                                <PlusOutlined />
-                            </button>
-                        </Upload>
+                        <Form.Item<any>
+                            name="symbol_thuoc_tinh">
+                            <Upload
+                                defaultFileList={[{
+                                    uid: '-1',
+                                    name: 'image.jpg',
+                                    status: 'done',
+                                    url: data?.symbol_thuoc_tinh,
+                                }]}
+                                listType="picture-card"
+                                beforeUpload={() => false}
+                                onChange={handleImageChange}
+                                className="mt-2"
+                                maxCount={1}>
+                                <button type="button">
+                                    <FilePenLine />
+                                </button>
+                            </Upload>
+                        </Form.Item>
                     }
                     <Form.Item label={null}>
                         <Button type="primary" htmlType="submit" className="translate-x-[120%]">
